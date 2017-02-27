c50a10862cb026cb365ef39087b585f6057a7f8d
Hanbyul Kim <hahanbyul@gmail.com>
2017-02-27 15:27:31 +0900

    [FIX] memoization

--- 2017-03-1W/TILING2.py
+++ 2017-03-1W/TILING2.py
@@ -11,14 +11,19 @@ class Tiling:
         self.cache = [-1 for _ in range(n)]
     
     def compute(self, n):
+        if self.cache[n-1] != -1: 
+            return self.cache[n]
+        
         if n <= 1: return 1
         if n == 2: return 2
         
-        return self.compute(n - 3) + 2*self.compute(n - 2)
+        ret = self.compute(n - 3) + 2*self.compute(n - 2)
+        self.cache[n-1] = ret
+        return ret
 
 def main():
     global cache
-    N = 10
+    N = 7
     t = Tiling(N)
     print(t.compute(N))    
 
