7a0f8ec58b2712b489596582035daadb8957c902
Hanbyul Kim <hahanbyul@gmail.com>
2017-02-28 15:08:18 +0900

    [ADD] test check_diag

--- 2017-03-1W/test_NQueen.py
+++ 2017-03-1W/test_NQueen.py
@@ -4,3 +4,9 @@ def test_print():
     nqueen = NQUEEN(4)
     assert nqueen.to_board([0,1,2,3]) == ["Q...", ".Q..", "..Q.", "...Q"]
     assert nqueen.to_board([3,1,2,0]) == ["...Q", ".Q..", "..Q.", "Q..."]
+
+def test_check_diag():
+    nqueen = NQUEEN(4)
+    assert nqueen.check_diag([1, 3], 2) == False
+    assert nqueen.check_diag([1, 3], 0) == True
+
