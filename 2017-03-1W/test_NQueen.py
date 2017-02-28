325e380e16ab902fe6fc43d43545bca23c0670fd
Hanbyul Kim <hahanbyul@gmail.com>
2017-02-28 15:28:51 +0900

    [ADD] test_answers

--- 2017-03-1W/test_NQueen.py
+++ 2017-03-1W/test_NQueen.py
@@ -2,6 +2,7 @@ from NQUEEN import NQUEEN
 
 def test_print():
     nqueen = NQUEEN(4)
+    assert nqueen.to_board([]) == []
     assert nqueen.to_board([0,1,2,3]) == ["Q...", ".Q..", "..Q.", "...Q"]
     assert nqueen.to_board([3,1,2,0]) == ["...Q", ".Q..", "..Q.", "Q..."]
 
@@ -12,3 +13,15 @@ def test_is_diag():
     assert nqueen.is_diag([1, 3, 0], 2) == False
     assert nqueen.is_diag([1, 3, 0], 0) == False
 
+def test_no_answer():
+    nqueen = NQUEEN(2)
+    nqueen.find([])
+    assert nqueen.answers == []
+    nqueen2 = NQUEEN(3)
+    nqueen2.find([])
+    assert nqueen.answers == []
+
+def test_answer():
+    nqueen = NQUEEN(4)
+    nqueen.find([])
+    assert nqueen.answers == [[".Q..", "...Q", "Q...", "..Q."], ["..Q.", "Q...", "...Q", ".Q.."]]
