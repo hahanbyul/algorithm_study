55994f2a993612c38f0d96cd7eef4a0f02459696
Hanbyul Kim <hahanbyul@gmail.com>
2017-03-02 15:41:17 +0900

    [ADD] parse_board

--- 2017-03-1W/TRIPATHCNT.py
+++ 2017-03-1W/TRIPATHCNT.py
@@ -5,13 +5,15 @@ Created on Tue Feb 28 13:33:55 2017
 
 @author: hanbyulkim
 """
+import sys
+rl = lambda: sys.stdin.readline()
 
 class TriPath:
     def __init__(self, N, board):
         self.N = N
         self.cache = [[-1 for _ in range(N)] for _ in range(N)]
         self.isMaxPath = False
-        self.board = []
+        self.board = board
     
     def path(self, y, x):
         return 0
@@ -29,3 +31,21 @@ class TriPath:
         if   left == right: return self.path_num(y+1, x) + self.path_num(y+1, x+1)
         elif left >  right: return self.path_num(y+1, x)
         elif left <  right: return self.path_num(y+1, x+1)
+
+def parse_board(n):
+    board = [[-1 for _ in range(n)] for _ in range(n)]
+    for row in range(n):
+        board[row][:row+1] = (int(num) for num in rl().split())
+    return board
+
+def main():
+    #C = int(rl())
+    C = 1
+
+    for _ in range(C):
+        n = int(rl())
+        print(parse_board(n))
+    return 0
+
+if __name__ == '__main__':
+    main()
