a268f4199f1a599c55b000b08b1213d2ed7aa816
Hanbyul Kim <hahanbyul@gmail.com>
2017-03-18 19:49:27 +0900

    [FIX] test -> OK except last answer

--- 2017-03-2W/ITES.py
+++ 2017-03-2W/ITES.py
@@ -8,3 +8,29 @@ class ITES:
             A_i = (A_i*214013 + 2531011 ) % 2**32
             self.A.append(A_i % 10000 + 1)
             i = i + 1
+
+    def find_seq(self, seq, K):
+        answers = set()
+        partial_seq = list()
+        partial_sum = 0
+
+        while seq:
+            while partial_sum < K:
+                partial_sum += seq[0]
+                partial_seq.append(seq.pop(0))
+                print("sum: ", partial_sum)
+                print("seq: ", partial_seq)
+
+            if partial_sum == K:
+                answers.add(tuple(partial_seq))
+                print("answers: ", answers)
+
+                partial_sum -= partial_seq[0]
+                partial_seq.pop(0)
+
+                continue
+
+            while partial_sum > K:
+                partial_sum -= partial_seq.pop(0)
+                print("sum: ", partial_sum)
+                print("seq: ", partial_seq)
--- 2017-03-2W/test_ITES.py
+++ 2017-03-2W/test_ITES.py
@@ -3,3 +3,9 @@ from ITES import ITES
 def test_init():
     ites = ITES(5)
     assert ites.A == [1983, 8791, 4770, 7665, 3188]
+
+def test_find():
+    ites = ITES(5)
+    ites.find_seq([1,4,2,1,4,3,1,6], 7) 
+    assert True
+
