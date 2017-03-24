58212e84bba0450e9a8adf1808639a5e0f1d5369
Hanbyul Kim <hahanbyul@gmail.com>
2017-03-24 17:34:08 +0900

    [FIX] FORTRESS: testing...

--- 2017-03-3W/test_fortress.py
+++ 2017-03-3W/test_fortress.py
@@ -1,4 +1,4 @@
-#from FORTRESS import Fortress
+from FORTRESS import Fortress
 from FORTRESS import Wall
 
 w1 = Wall("21 15 20")
@@ -15,11 +15,22 @@ def test_is_inside():
 def test_wall_operators():
     assert w2 in w1                 # contains
     assert w1 == Wall("21 15 20")   # eq 
-    assert w1 < w2                  # lt
+    assert w2 < w1                  # lt
     assert not w4 < w3
 
 def test_sort():
-    assert sorted([w2, w3, w1, w4]) == [w1, w2, w3, w4]
+    assert sorted([w2, w3, w1, w4]) == [w3, w4, w2, w1]
 
+def test_fortress():
+    f = Fortress()
+    f.read_string('21 15 20\n15 15 10\n13 12 5\n12 12 3\n19 19 2\n30 24 5\n32 10 7\n32 9 4')
+    assert str(f.walls) == '[(21, 15, 20), (15, 15, 10), (32, 10, 7), (13, 12, 5), (30, 24, 5), (32, 9, 4), (12, 12, 3), (19, 19, 2)]'
+
+    tree = f.make_tree()
+    assert [node.wall for node in tree.inside_walls] == [(15, 15, 10), (32, 10, 7), (32, 24, 5)]
+
+"""
 def test_tree():
     assert tree == '(21, 15, 20) - [(15, 15, 10) - [(13, 12, 5) - [(12, 12, 3)], (19, 19, 2)], (32, 10, 7) - [(32, 9, 4)], (30, 24, 5)]'
+"""
+
