c415866671cf169e5f95457f54d1a9826270b9c4
Hanbyul Kim <hahanbyul@gmail.com>
2017-04-01 01:28:29 +0900

    [FIX] CHECK_BST: re-coding, but failed

--- 2017-04-1W/CHECK_BST.py
+++ 2017-04-1W/CHECK_BST.py
@@ -1,13 +1,22 @@
-class Node:
-    children = set()
-    def __init__(self, string):
-        self.L, self.R, self.K = [int(i) for i in string.split()]
-        if self.L != 0: Node.children.add(self.L)
-        if self.R != 0: Node.children.add(self.R)
+class Check_BST:
+    def __init__(self):
+        self.node_list = list()
+        self.children = set()
+
+    def add_node(self, string):
+        node = Node(string)
+        self.node_list.append(node)
+
+        if node.L != 0: self.children.add(node.L)
+        if node.R != 0: self.children.add(node.R)
 
     def find_root(self, N):
-        root = set(range(1, N+1)).difference(Node.children)
+        root = set(range(1, N+1)).difference(self.children)
         return root.pop() if len(root) == 1 else None
+
+class Node:
+    def __init__(self, string):
+        self.L, self.R, self.K = [int(i) for i in string.split()]
 
     def is_bst(self, node_list):
         if self.L == 0 and self.R == 0:
@@ -20,21 +29,23 @@ class Node:
         return ret
 
 def main():
-    node_list = list()
 
     C = int(input())
     for _ in range(C):
 
         N = int(input())
-        for _ in range(N):
-            node_list.append(Node(input()))
+        check = Check_BST()
 
-        root = Node.find_root(None, N)
-        if root == None:
+        for _ in range(N):
+            check.add_node(input())
+
+        root_index = check.find_root(N)
+        if root_index == None:
             print("NO")
             continue
 
-        if node_list[root].is_bst(node_list):
+        root = check.node_list[root_index-1]
+        if root.is_bst(check.node_list):
             print("YES")
         else:
             print("NO")
--- 2017-04-1W/test_check_bst.py
+++ 2017-04-1W/test_check_bst.py
@@ -1,7 +1,19 @@
+from CHECK_BST import Check_BST
 from CHECK_BST import Node
 
 def test_find_root():
-    Node.children = set([1, 2, 3, 4])
-    assert Node.find_root(None, 5) == 5
-    assert Node.find_root(None, 10) == None
+    #Node.children = set([1, 2, 3, 4])
+    #assert Node.find_root(None, 5) == 5
+    #assert Node.find_root(None, 10) == None
 
+    strings = "3 2 4\n0 0 5\n4 5 2\n0 0 1\n0 0 3"
+    check = Check_BST()
+    for s in strings.split(sep="\n"):
+        check.add_node(s)
+    assert check.find_root(5) == 1
+
+    strings2 = "0 0 2\n4 3 3\n1 0 3\n0 0 1"
+    check2 = Check_BST()
+    for s in strings2.split(sep="\n"):
+        check2.add_node(s)
+    assert check2.find_root(4) == 2
