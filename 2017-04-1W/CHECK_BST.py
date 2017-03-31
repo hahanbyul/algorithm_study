81304f59ab6c6f01e5e2178a769b58387f51934d
Hanbyul Kim <hahanbyul@gmail.com>
2017-04-01 02:30:34 +0900

    [FIX] CHECK_BST: check left and right separately, read_string, is_bst

--- 2017-04-1W/CHECK_BST.py
+++ 2017-04-1W/CHECK_BST.py
@@ -1,7 +1,12 @@
 class Check_BST:
-    def __init__(self):
+    def __init__(self, N):
         self.node_list = list()
         self.children = set()
+        self.N = N
+
+    def read_string(self, string):
+        for s in string.split(sep="\n"):
+            self.add_node(s)
 
     def add_node(self, string):
         node = Node(string)
@@ -14,18 +19,41 @@ class Check_BST:
         root = set(range(1, N+1)).difference(self.children)
         return root.pop() if len(root) == 1 else None
 
+    def is_bst(self):
+        root_index = self.find_root(self.N)
+        if root_index == None:
+            return False
+
+        root = self.node_list[root_index-1]
+        if root.is_bst(self.node_list, list()):
+            return True
+        else:
+            return False
+
 class Node:
     def __init__(self, string):
         self.L, self.R, self.K = [int(i) for i in string.split()]
 
-    def is_bst(self, node_list):
+    def __repr__(self):
+        return "(%d, %d, %d)" % (self.L, self.R, self.K)
+
+    def is_bst(self, node_list, key_list):
+        if self.K in key_list:
+            return False
+        key_list.append(self.K)
+
         if self.L == 0 and self.R == 0:
             return True
 
         ret = True
-        left  = node_list[self.L-1]
-        right = node_list[self.R-1]
-        ret = ret and left.K < self.K and right.K > self.K and left.is_bst(node_list) and right.is_bst(node_list)
+        if self.L != 0:
+            left  = node_list[self.L-1]
+            ret = ret and left.K < self.K and left.is_bst(node_list, key_list)
+
+        if self.R != 0:
+            right = node_list[self.R-1]
+            ret = ret and right.K > self.K and right.is_bst(node_list, key_list)
+
         return ret
 
 def main():
@@ -34,21 +62,16 @@ def main():
     for _ in range(C):
 
         N = int(input())
-        check = Check_BST()
+        check = Check_BST(N)
 
         for _ in range(N):
             check.add_node(input())
 
-        root_index = check.find_root(N)
-        if root_index == None:
-            print("NO")
-            continue
-
-        root = check.node_list[root_index-1]
-        if root.is_bst(check.node_list):
+        if check.is_bst():
             print("YES")
         else:
             print("NO")
+
 
 if __name__ == '__main__':
     main()
--- 2017-04-1W/test_check_bst.py
+++ 2017-04-1W/test_check_bst.py
@@ -1,19 +1,27 @@
 from CHECK_BST import Check_BST
 from CHECK_BST import Node
 
+strings = "3 2 4\n0 0 5\n4 5 2\n0 0 1\n0 0 3"
+check = Check_BST(5)
+check.read_string(strings)
+
+strings2 = "0 0 2\n4 3 3\n1 0 3\n0 0 1"
+check2 = Check_BST(4)
+check2.read_string(strings2)
+
+strings3 = "2 3 3\n0 4 1\n0 0 4\n0 0 3"
+check3 = Check_BST(4)
+check3.read_string(strings3)
+
 def test_find_root():
     #Node.children = set([1, 2, 3, 4])
     #assert Node.find_root(None, 5) == 5
     #assert Node.find_root(None, 10) == None
 
-    strings = "3 2 4\n0 0 5\n4 5 2\n0 0 1\n0 0 3"
-    check = Check_BST()
-    for s in strings.split(sep="\n"):
-        check.add_node(s)
     assert check.find_root(5) == 1
-
-    strings2 = "0 0 2\n4 3 3\n1 0 3\n0 0 1"
-    check2 = Check_BST()
-    for s in strings2.split(sep="\n"):
-        check2.add_node(s)
     assert check2.find_root(4) == 2
+
+def test_is_bst():
+    assert check.is_bst() == True
+    assert check2.is_bst() == False
+    assert check3.is_bst() == False
