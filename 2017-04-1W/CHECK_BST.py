83b6a140923491909c690f2bd0dbd46c439e5a49
Hanbyul Kim <hahanbyul@gmail.com>
2017-04-01 00:02:13 +0900

    [FIX] CHECK_BST: ex -> PASS, online judge -> FAIL

--- 2017-04-1W/CHECK_BST.py
+++ 2017-04-1W/CHECK_BST.py
@@ -9,6 +9,16 @@ class Node:
         root = set(range(1, N+1)).difference(Node.children)
         return root.pop() if len(root) == 1 else None
 
+    def is_bst(self, node_list):
+        if self.L == 0 and self.R == 0:
+            return True
+
+        ret = True
+        left  = node_list[self.L-1]
+        right = node_list[self.R-1]
+        ret = ret and left.K < self.K and right.K > self.K and left.is_bst(node_list) and right.is_bst(node_list)
+        return ret
+
 def main():
     node_list = list()
 
@@ -19,6 +29,15 @@ def main():
         for _ in range(N):
             node_list.append(Node(input()))
 
+        root = Node.find_root(None, N)
+        if root == None:
+            print("NO")
+            continue
+
+        if node_list[root].is_bst(node_list):
+            print("YES")
+        else:
+            print("NO")
 
 if __name__ == '__main__':
     main()
