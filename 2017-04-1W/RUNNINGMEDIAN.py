3e9040385c06f68adf9b814efd05ebac3522835c
Hanbyul Kim <hahanbyul@gmail.com>
2017-04-06 20:08:25 +0900

    [ADD] MEDIAN: main, generator, put_inorder, ex. -> PASS

--- 2017-04-1W/RUNNINGMEDIAN.py
+++ 2017-04-1W/RUNNINGMEDIAN.py
@@ -1,10 +1,42 @@
+from math import log2
+from math import inf
+from math import ceil
+
 class Heap:
     def __init__(self):
         self.heap = list()
-        self.heap.append(-1)
+        self.heap.append(inf)
+
+    def generator(self, a, b):
+        A = 1983
+        yield A
+        while True:
+            A = (A*a + b) % 20090711
+            yield A
+
+    def put_inorder(self, key):
+        N = len(self.heap)
+        self.put(key)
+
+        i = N
+        parent = int(i/2)
+        while i >= 2:
+            #print("i: ", i, ", heal[i]", self.heap[i])
+            while self.heap[i-1] < self.heap[i]:
+                self.heap[i-1], self.heap[i] = self.heap[i], self.heap[i-1]
+                i -= 1
+            i = parent
+            parent = int(i/2)
+
+    def get_median(self):
+        #print(self.heap)
+        med_idx = ceil(len(self.heap) / 2)
+        #print(self.heap[med_idx])
+        return self.heap[med_idx]
 
     def put(self, key):
-        i = len(self.heap)
+        N = len(self.heap)
+        i = N
         parent = int(i/2)
         self.heap.append(key)
 
@@ -41,3 +73,20 @@ class Heap:
 
     def get_heap(self):
         return self.heap[1:]
+
+def main():
+    C = int(input())                 # num. of cases
+    for _ in range(C):
+        N, a, b = [int(i) for i in input().split()]
+
+        h = Heap()
+        gen = h.generator(a, b)
+
+        part_sum = 0
+        for _ in range(N):
+            h.put_inorder(gen.__next__())
+            part_sum += h.get_median()
+        print(part_sum % 20090711)
+
+if __name__ == '__main__':
+    main()
--- 2017-04-1W/test_running_median.py
+++ 2017-04-1W/test_running_median.py
@@ -16,3 +16,30 @@ def test_heap():
     print(h.delete_max())
     print(h.delete_max())
     print(h.delete_max())
+
+def test_put_inorder():
+    h = Heap()
+    h.put_inorder(0)
+    assert h.get_heap() == [0]
+    h.put_inorder(1)
+    h.put_inorder(2)
+    assert h.get_heap() == [2, 1, 0]
+    h.put_inorder(3)
+    assert h.get_heap() == [3, 2, 1, 0]
+    h.put_inorder(0)
+    assert h.get_heap() == [3, 2, 1, 0, 0]
+    h.put_inorder(2)
+    assert h.get_heap() == [3, 2, 2, 1, 0, 0]
+
+def test_ex():
+    h = Heap()
+    h.put_inorder(3)
+    assert h.get_median() == 3
+    h.put_inorder(1)
+    assert h.get_median() == 1
+    h.put_inorder(5)
+    assert h.get_median() == 3
+    h.put_inorder(4)
+    assert h.get_median() == 3
+    h.put_inorder(2)
+    assert h.get_median() == 3
