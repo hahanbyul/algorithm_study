9331d9e34244845ddd54d5b02f75de4ba710a45c
Hanbyul Kim <hahanbyul@gmail.com>
2017-04-11 01:34:59 +0900

    [ADD] DICTIONARY: make_init_graph

--- 2017-04-2W/DICTIONARY.py
+++ 2017-04-2W/DICTIONARY.py
@@ -1,16 +1,50 @@
 class Dictionary:
     def __init__(self):
-        self.words = list()
+        self.words_list = list()
+        self.vertex = dict()
 
     def read_words(self, N):
         for _ in range(N):
-            self.words.append(input())
+            word = input()
+            self.make_init_graph(word)
+            self.words_list.append(word)
+
+    def make_init_graph(self, word):
+        if len(self.words_list) == 0:
+            return
+
+        last_word = self.words_list[-1]
+        if last_word[0] == word[0]:
+            return
+
+        adjacent = self.get_adjacent(last_word[0])
+        adjacent.append(word[0])
+
+    def get_adjacent(self, char):
+        adjacent = self.vertex.get(char, None)
+        if adjacent is None:
+            adjacent = list()
+            self.vertex[char] = adjacent
+
+        return adjacent
 
     def read_words_with_string(self, string):
-        self.words = string.split("\n")
+        for word in string.split("\n"):
+            self.make_init_graph(word)
+            self.words_list.append(word)
 
     def find_alphabet_order(self):
         pass
+        
+
+    def dfs(self, index, char, visited):
+        if visited[-1] != self.words_list[index + 1][len(visited)-1]:
+            return
+
+class Vertex:
+    def __init__(self, char):
+        self.char = char
+        self.edges = list()
 
 def main():
     C = int(input())
--- 2017-04-2W/test_dictionary.py
+++ 2017-04-2W/test_dictionary.py
@@ -3,5 +3,12 @@ from DICTIONARY import Dictionary
 d = Dictionary()
 d.read_words_with_string("ba\naa\nab")
 
+d2 = Dictionary()
+d2.read_words_with_string("ba\naa\ncb")
+
 def test_read_input():
-    assert d.words == ["ba", "aa", "ab"]
+    assert d.words_list == ["ba", "aa", "ab"]
+
+def test_make_init_graph():
+    assert d.vertex == {"b": ["a"]}
+    assert d2.vertex == {"b": ["a"], "a": ["c"]}
