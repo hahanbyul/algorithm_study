a8ab081a3071be3f03f9b61bb71bdd44aa049f11
Hanbyul Kim <hahanbyul@gmail.com>
2017-04-11 16:51:20 +0900

    [FIX] DICTIONARY: dfs -> cycle_check, get_answer

--- 2017-04-2W/DICTIONARY.py
+++ 2017-04-2W/DICTIONARY.py
@@ -1,50 +1,93 @@
+import string
+
 class Dictionary:
     def __init__(self):
         self.words_list = list()
         self.vertex = dict()
+        self.is_visited = dict()
 
     def read_words(self, N):
         for _ in range(N):
             word = input()
-            self.make_init_graph(word)
+            #self.make_init_graph(word)
             self.words_list.append(word)
 
-    def make_init_graph(self, word):
-        if len(self.words_list) == 0:
-            return
-
-        last_word = self.words_list[-1]
-        if last_word[0] == word[0]:
-            return
-
-        adjacent = self.get_adjacent(last_word[0])
-        adjacent.append(word[0])
+    def read_words_with_string(self, string):
+        for word in string.split("\n"):
+            #self.make_init_graph(word)
+            self.words_list.append(word)
 
     def get_adjacent(self, char):
         adjacent = self.vertex.get(char, None)
         if adjacent is None:
-            adjacent = list()
+            adjacent = set()
             self.vertex[char] = adjacent
 
         return adjacent
+    
+    def find_diff_idx(self, this_word, next_word):
+        i = 0
+        while i < len(this_word) and this_word[i] == next_word[i]:
+            i += 1
+        return i
 
-    def read_words_with_string(self, string):
-        for word in string.split("\n"):
-            self.make_init_graph(word)
-            self.words_list.append(word)
+    def find_graph(self):
+        for i, this_word in enumerate(self.words_list[:-1]):
+            next_word = self.words_list[i+1]
 
-    def find_alphabet_order(self):
-        pass
-        
+            diff_idx = self.find_diff_idx(this_word, next_word)
+            if diff_idx == len(this_word):
+                continue
 
-    def dfs(self, index, char, visited):
-        if visited[-1] != self.words_list[index + 1][len(visited)-1]:
+            adjacent = self.get_adjacent(this_word[diff_idx])
+            adjacent.add(next_word[diff_idx])
+
+        return self.vertex
+
+    def foo(self):
+        try:
+            char_list = list()
+            for key in self.vertex.keys():
+                self.dfs(key, char_list, list()) 
+                if not self.is_visited.get(key, False):
+                    char_list.append(key)
+                self.is_visited[key] = True
+                print(f'is_visited: {self.is_visited}')
+                print(f'char_list: {char_list}')
+        except Exception:
+            print("INVALID HYPOTHESIS")
+        self.char_list = char_list
+        self.char_list.reverse()
+        return self.char_list
+
+    def dfs(self, char, char_list, cycle_check):
+        print(f'dfs({char})')
+        if self.is_visited.get(char, False) or char not in self.vertex.keys():
+            print('return')
             return
 
-class Vertex:
-    def __init__(self, char):
-        self.char = char
-        self.edges = list()
+        cycle_check.append(char)
+
+        for adj in self.get_adjacent(char):
+            print(f'adj: {self.get_adjacent(char)}')
+            if adj in cycle_check:
+                raise Exception
+
+            self.dfs(adj, char_list, cycle_check)
+            if not self.is_visited.get(adj, False):
+                char_list.append(adj)
+            self.is_visited[adj] = True
+
+            print(f'is_visited: {self.is_visited}')
+            print(f'char_list: {char_list}')
+
+    def get_answer(self):
+        self.find_graph()
+        self.foo()
+        if sorted(self.char_list) == self.char_list:
+            return string.ascii_lowercase
+        else:
+            return "".join(self.char_list + [char for char in string.ascii_lowercase if char not in self.char_list])
 
 def main():
     C = int(input())
--- 2017-04-2W/test_dictionary.py
+++ 2017-04-2W/test_dictionary.py
@@ -1,14 +1,51 @@
 from DICTIONARY import Dictionary
+import string
 
-d = Dictionary()
-d.read_words_with_string("ba\naa\nab")
+d = list()
+for i in range(4):
+    d.append(Dictionary())
 
-d2 = Dictionary()
-d2.read_words_with_string("ba\naa\ncb")
+d[0].read_words_with_string("ba\naa\nab")
+d[1].read_words_with_string("aa\nba\nbc")
+d[2].read_words_with_string("gg\nkia\nlotte\nlg\nhanhwa")
 
 def test_read_input():
-    assert d.words_list == ["ba", "aa", "ab"]
+    assert d[0].words_list == ["ba", "aa", "ab"]
 
+'''
 def test_make_init_graph():
-    assert d.vertex == {"b": ["a"]}
-    assert d2.vertex == {"b": ["a"], "a": ["c"]}
+    assert d[0].vertex == {"b": ["a"]}
+    assert d[1].vertex == {"b": ["a"], "a": ["c"]}
+'''
+
+def test_find_diff_idx():
+    assert d[0].find_diff_idx("fa", "gb") == 0
+    assert d[0].find_diff_idx("fa", "fb") == 1
+    assert d[0].find_diff_idx("fa", "faa") == 2
+
+def test_find_graph():
+    assert d[0].find_graph() == {'b': {'a'}, 'a': {'b'}}
+    assert d[1].find_graph() == {'a': {'b', 'c'}}
+    assert d[2].find_graph() == {'g': {'k'}, 'k': {'l'}, 'o': {'g'}, 'l': {'h'}}
+
+"""
+def test_foo():
+    try: 
+        d[0].foo()
+    except Exception:
+        assert True
+    assert d[1].foo() == ['b', 'c', 'a'].reverse()
+    assert d[2].foo() == ['h', 'l', 'k', 'g', 'o'].reverse()
+
+    d[3].vertex = {'a': ['b'], 'b': ['c', 'd'], 'd': 'c'}
+    assert d[3].foo() == ['c', 'd', 'b', 'a'].reverse()
+"""
+
+def test_get_answer():
+    dd = Dictionary()
+    dd.read_words_with_string("gg\nkia\nlotte\nlg\nhanhwa")
+    dd.get_answer() == 'ogklhabcdefijmnpqrstuvwxyz'
+
+    ddd = Dictionary()
+    ddd.read_words_with_string("dictionary\nenglish\nis\nordered\nordinary\nthis")
+    ddd.get_answer() == string.ascii_lowercase
