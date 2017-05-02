eca9fcec9db714af94ace1d1841abfbd104a45d2
Hanbyul Kim <hahanbyul@gmail.com>
2017-05-02 17:12:31 +0900

    [ADD] CHEESE: color_opened_hole, solve | ex -> PASS

--- 2017-04-4W/CHEESE.py
+++ 2017-04-4W/CHEESE.py
@@ -26,6 +26,8 @@ class Cheese:
         self.row_hole = dict()
         self.col_hole = dict()
 
+        self.opened_holes = list()
+
     def parse_board(self, board_str):
         return [[int(i) for i in row_str.split()]for row_str in board_str.split('\n')]
 
@@ -48,18 +50,6 @@ class Cheese:
             
     def col(self, j):
         return [row[j] for row in self.board]
-            
-    def row_edge_iter(self):
-        for i, row in enumerate(self.board):
-            if self.is_row_empty[i]:
-                continue
-                            
-            row = self.row(i)
-            self.row_first[i] = self.first_cheese(row, self.row_first.get(i, 0))
-            self.row_last[i]  = self.last_cheese(row, self.row_last.get(i, len(row)-1))
-            
-            if self.row_first[i] == len(row)-1:
-                self.is_row_empty[i] = True
 
     def edge_iter(self, mode):
         if mode == 'row':
@@ -119,33 +109,17 @@ class Cheese:
             found = list()
             while hole_right <= last[i]:
                 hole_left = self.last_cheese(line, hole_right)
-                found.append((hole_left, hole_right))
+                found.append(Hole((hole_left, hole_right), mode, i))
                 hole_right = self.first_cheese(line, hole_right)
 
             if len(found) > 0:
                 hole_list[i] = found
 
-    def row_hole_iter(self):
-        for i, row in enumerate(self.board):
-            if self.is_row_empty[i]:
-                continue
-                            
-            row = self.row(i)
-            hole_right = self.first_cheese(row, self.row_first[i])
-            holes_list = list()
-            while hole_right <= self.row_last[i]:
-                hole_left = self.last_cheese(row, hole_right)
-                holes_list.append((hole_left, hole_right))
-                hole_right = self.first_cheese(row, hole_right)
-
-            if len(holes_list) > 0:
-                self.row_hole[i] = holes_list
-            
     def get_row_hole(self, i):
-        return self.row_hole.get(i, None)
+        return [(hole.left, hole.right) for hole in self.row_hole.get(i, [])]
 
     def get_col_hole(self, i):
-        return self.col_hole.get(i, None)
+        return [(hole.left, hole.right) for hole in self.col_hole.get(i, [])]
                     
     def has_hole(self, i):
         return len(self.row_hole[i]) > 0
@@ -157,7 +131,10 @@ class Cheese:
         if next_left <= cur_right:
             self.explore_row_hole(self, idx + direction, direction)
 
-    def color_row_edge(self, board):
+    def color_row_edge(self, board=None):
+        if board is None:
+            board = self.board
+
         for i in range(len(board)):
             if self.is_row_empty[i]:
                 continue
@@ -168,21 +145,25 @@ class Cheese:
                 board[i][edge[1]] = 6
         return board
 
-    def color_row_hole(self, board):
+    def color_row_hole(self, board=None):
+        if board is None:
+            board = self.board
+
         for i in range(len(board)):
             if self.is_row_empty[i]:
                 continue
 
             hole = self.get_row_hole(i)
-            if hole is None:
-                continue
-
             for h in hole:
                 board[i][h[0]] = 7
                 board[i][h[1]] = 7
+
         return board
 
-    def color_col_edge(self, board):
+    def color_col_edge(self, board=None):
+        if board is None:
+            board = self.board
+
         for i in range(len(board[0])):
             if self.is_col_empty[i]:
                 continue
@@ -193,21 +174,47 @@ class Cheese:
                 board[edge[1]][i] = 6
         return board
 
-    def color_col_hole(self, board):
+    def color_col_hole(self, board=None):
+        if board is None:
+            board = self.board
+
         for i in range(len(board[0])):
             if self.is_col_empty[i]:
                 continue
 
             hole = self.get_col_hole(i)
-            if hole is None:
-                continue
-
             for h in hole:
                 board[h[0]][i] = 7
                 board[h[1]][i] = 7
         return board
 
-    def print_melting_cheese(self, board):
+    def color_opened_hole(self, board=None):
+        if board is None:
+            board = self.board
+
+        for hole in self.opened_holes:
+            self.color_hole_iter(board, hole)
+
+        return board
+
+    def color_hole_iter(self, board, hole):
+        hole.colored = True
+
+        if hole.mode == 'row':
+            board[hole.idx][hole.left] = 6
+            board[hole.idx][hole.right] = 6
+        elif hole.mode == 'col':
+            board[hole.left][hole.idx] = 6
+            board[hole.right][hole.idx] = 6
+
+        for next_hole in hole.adj:
+            if not next_hole.colored:
+                self.color_hole_iter(board, next_hole)
+
+    def print_melting_cheese(self, board=None):
+        if board is None:
+            board = self.board
+
         for row in board:
             print()
             for e in row:
@@ -220,15 +227,82 @@ class Cheese:
                 elif e == 7:
                     print(colorful.blue('h'), end=' ')
 
-    def is_opened_row_hole(self, row_hole):
-        upper_edge = get_row_edge(row_hole.idx - 1)
-        lower_edge = get_row_edge(row_hole.idx + 1)
+    def is_opened_hole(self, hole):
+        if hole.mode == 'row':
+            get_edge = self.get_row_edge
+        elif hole.mode == 'col':
+            get_edge = self.get_col_edge
 
-        if upper_edge is None or upper_edge[0] > row_hole.left + 1 or upper_edge[1] < row_hole.right - 1:
-            return True
+        upper_edge = get_edge(hole.idx - 1)
+        lower_edge = get_edge(hole.idx + 1)
 
-        if lower_edge is None or lower_edge[0] > row_hole.left + 1 or lower_edge[1] < row_hole.right - 1:
+        if upper_edge is None or upper_edge[0] > hole.left + 1 or upper_edge[1] < hole.right - 1:
             return True
+        elif lower_edge is None or lower_edge[0] > hole.left + 1 or lower_edge[1] < hole.right - 1:
+            return True
+        else: 
+            return False
+
+    def update_hole_status(self, mode):
+        if mode == 'row':
+            is_empty = self.is_row_empty
+            hole_list = self.row_hole
+        elif mode == 'col':
+            is_empty = self.is_col_empty
+            hole_list = self.col_hole
+
+        for i in range(len(is_empty)):
+            if is_empty[i]:
+                continue
+
+            for hole in hole_list.get(i, []):
+                if self.is_opened_hole(hole):
+                    self.opened_holes.append(hole)
+
+                next_holes = hole_list.get(i+1, [])
+                for another in next_holes:
+                    if hole.is_connected(another):
+                        hole.add_neighbor(another)
+
+    def solve(self):
+        hour = 0
+        while all(is_row_empty == True):
+            self.solve_iter()
+            hour += 1
+            count = self.count_melting()
+
+        print(hour)
+        print(count)
+        
+
+    def solve_iter(self):
+        self.edge_iter('row')
+        self.edge_iter('col')
+
+        self.hole_iter('row')
+        self.hole_iter('col')
+
+        self.update_hole_status('row')
+        self.update_hole_status('col')
+
+        self.color_row_edge(self.board)
+        self.color_col_edge(self.board)
+        self.color_opened_hole(self.board)
+
+        self.row_hole = dict()
+        self.col_hole = dict()
+        self.opened_holes = list()
+
+    def count_melting(self):
+        count = 0 
+        for i, row in enumerate(self.board):
+            for j, e in enumerate(row):
+                if e == 6:
+                    count += 1
+                    self.board[i][j] = 0
+
+        return count
+
 
 class Hole:
     def __init__(self, hole, mode, idx):
@@ -239,7 +313,30 @@ class Hole:
         self.idx  = idx
 
         self.adj = list()
+        self.colored = False
+
+    def __repr__(self):
+        #return f"({self.left},{self.right})"
+        neighbor_list = ",".join([f"({neighbor.left}, {neighbor.right})" for neighbor in self.adj])
+        return f"Hole(({self.left},{self.right}), {self.mode}, {self.idx}) - [{neighbor_list}]"
+
+    def __eq__(self, another):
+        return self.left == another.left and self.right == another.right and self.mode == another.mode and self.idx == another.idx
 
     def add_neighbor(self, another):
         self.adj.append(another)
         another.adj.append(self)
+
+    def is_connected(self, another):
+        set1 = set(range(self.left + 1, self.right))
+        set2 = set(range(another.left + 1, another.right))
+        return len(set1 & set2) > 0
+
+
+def main():
+    C = int(raw_input())
+    for _ in range(C):
+        pass
+
+if __name__ == '__main__'
+    main()
--- 2017-04-4W/test_cheese.py
+++ 2017-04-4W/test_cheese.py
@@ -1,4 +1,6 @@
 from CHEESE import Cheese
+from CHEESE import Hole
+import pprint as pp
 
 line1 = [0,1,0,1,1,0]
 board1 = [[0,0,0,0,0],
@@ -18,12 +20,12 @@ board_ex = "0 0 0 0 0 0 0 0 0 0 0 0\n0 0 0 0 0 0 0 0 0 0 0 0\n0 0 0 0 0 0 0 1 1 
             \n0 0 1 1 1 1 1 1 1 0 0 0\n0 0 1 1 1 1 1 1 1 0 0 0\n0 0 1 1 1 1 1 1 1 0 0 0\n0 0 1 1 1 1 1 1 1 0 0 0\n0 0 0 0 0 0 0 0 0 0 0 0"
 
 c = Cheese(board1)
-c.row_edge_iter()
-c.row_hole_iter()
+c.edge_iter('row')
+c.hole_iter('row')
 
 c2 = Cheese(board2)
-c2.row_edge_iter()
-c2.row_hole_iter()
+c2.edge_iter('row')
+c2.hole_iter('row')
 
 cheese_ex = Cheese(board_ex)
 
@@ -31,6 +33,8 @@ def test_col():
     assert c2.col(1) == [0, 1, 1, 1, 1, 0]
     assert c2.col(2) == [0, 1, 0, 1, 0, 0]
 
+def test_hole():
+    print(Hole((1,2), 'row', 3))
 
 def test_first_cheese():
     assert c.first_cheese(line1, 0) == 1
@@ -49,19 +53,19 @@ def test_col_edge():
     pass
 
 def test_has_row_hole():
-    assert c.get_row_hole(0) is None
+    assert c.get_row_hole(0) == []
     assert c.get_row_hole(1) == [(1,3)]
     assert c.get_row_hole(2) == [(1,3)]
-    assert c.get_row_hole(3) is None
-    assert c.get_row_hole(4) is None
-    assert c.get_row_hole(5) is None
+    assert c.get_row_hole(3) == []
+    assert c.get_row_hole(4) == []
+    assert c.get_row_hole(5) == []
 
-    assert c2.get_row_hole(0) is None
+    assert c2.get_row_hole(0) == []
     assert c2.get_row_hole(1) == [(2,4)]
     assert c2.get_row_hole(2) == [(1,4)]
-    assert c2.get_row_hole(3) is None
+    assert c2.get_row_hole(3) == []
     assert c2.get_row_hole(4) == [(1,3),(3,5)]
-    assert c2.get_row_hole(5) is None
+    assert c2.get_row_hole(5) == []
 
 def test_print_melting_cheese():
     cheese_ex.edge_iter('row')
@@ -80,26 +84,72 @@ def test_print_melting_cheese():
     # cheese_ex.row_edge_iter()
     # cheese_ex.row_hole_iter()
 
-def test_general_edge_iter():
-    cheese1 = Cheese(board1)
-    cheese2 = Cheese(board1)
+def test_is_opended_hole():
+    cheese_ex.edge_iter('row')
+    cheese_ex.hole_iter('row')
 
-    cheese1.row_edge_iter()
-    cheese2.edge_iter('row')
+    hole_list = cheese_ex.get_row_hole(3)
+    assert hole_list == [(3,7)]
+    assert cheese_ex.is_opened_hole(Hole(hole_list[0], 'row', 3))
 
-    assert cheese1.is_row_empty == cheese2.is_row_empty
-    assert cheese1.row_first == cheese2.row_first
-    assert cheese1.row_last == cheese2.row_last
+    hole_list = cheese_ex.get_row_hole(5)
+    assert not cheese_ex.is_opened_hole(Hole(hole_list[0], 'row', 5))
+    hole_list = cheese_ex.get_row_hole(6)
+    assert not cheese_ex.is_opened_hole(Hole(hole_list[0], 'row', 6))
+    hole_list = cheese_ex.get_row_hole(7)
+    assert not cheese_ex.is_opened_hole(Hole(hole_list[0], 'row', 7))
 
-def test_general_hole_iter():
-    cheese1 = Cheese(board1)
-    cheese2 = Cheese(board1)
+    cheese_ex.edge_iter('col')
+    cheese_ex.hole_iter('col')
 
-    cheese1.row_edge_iter()
-    cheese2.row_edge_iter()
+    hole_list = cheese_ex.get_col_hole(4)
+    assert not cheese_ex.is_opened_hole(Hole(hole_list[0], 'col', 4))
+    hole_list = cheese_ex.get_col_hole(5)
+    assert not cheese_ex.is_opened_hole(Hole(hole_list[0], 'col', 5))
+    hole_list = cheese_ex.get_col_hole(6)
+    assert not cheese_ex.is_opened_hole(Hole(hole_list[0], 'col', 6))
+    hole_list = cheese_ex.get_col_hole(7)
+    assert not cheese_ex.is_opened_hole(Hole(hole_list[0], 'col', 7))
+    hole_list = cheese_ex.get_col_hole(8)
+    assert cheese_ex.is_opened_hole(Hole(hole_list[0], 'col', 8))
 
-    cheese1.row_hole_iter()
-    cheese2.hole_iter('row')
+def test_connected():
+    hole1 = Hole((3,5),'row',1)
+    hole2 = Hole((4,8),'row',2)
+    assert not hole1.is_connected(hole2)
 
-    assert cheese1.is_row_empty == cheese2.is_row_empty
-    assert cheese1.row_hole == cheese2.row_hole
+def test_update_row_hole():
+    cheese_ex.edge_iter('row')
+    cheese_ex.hole_iter('row')
+
+    cheese_ex.update_hole_relation('row')
+    print(f'\nopened: {cheese_ex.opened_holes}')
+    print('row_holes: ')
+    pp.pprint(cheese_ex.row_hole)
+
+def test_update_col_hole():
+    cheese_ex.edge_iter('col')
+    cheese_ex.hole_iter('col')
+
+    cheese_ex.update_hole_relation('col')
+    print(f'\nopened: {cheese_ex.opened_holes}')
+    print('col_holes: ')
+    pp.pprint(cheese_ex.col_hole)
+
+def test_color_opened_holes():
+    cheese_ex.edge_iter('row')
+    cheese_ex.hole_iter('row')
+    cheese_ex.update_hole_status('row')
+
+    cheese_ex.edge_iter('col')
+    cheese_ex.hole_iter('col')
+    cheese_ex.update_hole_status('col')
+    cheese_ex.print_melting_cheese(cheese_ex.color_opened_hole(cheese_ex.board))
+
+def test_solve_iter():
+    cheese_ex.solve_iter()
+    print(cheese_ex.count_melting())
+    cheese_ex.solve_iter()
+    print(cheese_ex.count_melting())
+    cheese_ex.solve_iter()
+    cheese_ex.print_melting_cheese()
