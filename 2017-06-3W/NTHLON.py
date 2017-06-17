5003a5f235f287b1bece8675cedcfc7f86303828
Hanbyul Kim <hahanbyul@gmail.com>
2017-06-17 19:04:33 +0900

    [FIX] mid complete, argument change

--- 2017-06-3W/NTHLON.py
+++ 2017-06-3W/NTHLON.py
@@ -1,18 +1,18 @@
 # https://algospot.com/judge/problem/read/NTHLON
 import sys
 import heapq
+import math
 
 input = sys.stdin.readline
 
 class Nthlon:
-    def lin_comb(self, diff_arr, cost_arr):
-        diff_graph = [diff_arr for _ in range(len(diff_arr))]
-        cost_graph = [cost_arr for _ in range(len(cost_arr))]
-        max_limit = 100
+    def lin_comb(self, max_limit):
+        diff_graph = [self.diff_plus for _ in range(len(self.diff_plus))]
+        cost_graph = [self.cost_plus for _ in range(len(self.cost_plus))]
 
-        frontier = [ (cost, i, diff) for i, (diff, cost) in enumerate(zip(diff_arr, cost_arr)) ]
+        frontier = [ (cost, i, diff) for i, (diff, cost) in enumerate(zip(self.diff_plus, self.cost_plus)) ]
         heapq.heapify(frontier)
-        visited = {}
+        discovered = {}
         print(f'frontier: {frontier}')
 
         while len(frontier) > 0:
@@ -26,25 +26,70 @@ class Nthlon:
                 next_cost = cur_cost + cost
                 next_diff = cur_diff + diff
 
-                if not visited.get(next_diff, False):
+                if next_cost < discovered.get(next_diff, math.inf):
                     heapq.heappush(frontier, (next_cost, i, next_diff))
-                    visited[next_diff] = True
+                    discovered[next_diff] = next_cost
+
             #print(f'frontier: {frontier}')
 
-    def get_answer(self, diff_arr, cost_arr, goal_diff):
-        cost_arr = [cost for cost, diff in zip(cost_arr, diff_arr) if diff <= goal_diff]
-        diff_arr = [diff for diff in diff_arr if diff <= goal_diff]
-        print(f'diff: {diff_arr}')
-        print(f'cost: {cost_arr}')
+    def get_answer(self, goal_diff):
+        self.cost_minus = [cost for cost, diff in zip(self.cost_minus, self.diff_minus) if diff <= goal_diff]
+        self.diff_minus = [diff for diff in self.diff_minus if diff <= goal_diff]
 
+        diff_graph = [self.diff_minus for _ in range(len(self.diff_minus))]
+        cost_graph = [self.cost_minus for _ in range(len(self.cost_minus))]
 
+        frontier = [ (cost, i, diff) for i, (diff, cost) in enumerate(zip(self.diff_minus, self.cost_minus)) ]
+
+        heapq.heapify(frontier)
+        discovered = {}
+        print(f'frontier: {frontier}')
+
+        while len(frontier) > 0:
+            cur_cost, current, cur_diff = heapq.heappop(frontier)
+            print(cur_cost)
+
+            if goal_diff <= cur_diff:
+                return cur_cost
+
+            for i, (diff, cost) in enumerate(zip(diff_graph[current], cost_graph[current])):
+                next_cost = cur_cost + cost
+                next_diff = cur_diff + diff
+
+                print(f'[next] cost: {next_cost}, diff: {next_diff}')
+
+                if next_diff <= goal_diff and next_cost < discovered.get(next_diff, math.inf):
+                    heapq.heappush(frontier, (next_cost, i, next_diff))
+                    discovered[next_diff] = next_cost
+            print(f'frontier: {frontier}')
 
 def main():
     C = int(input())
     for _ in range(C):
         M = int(input())
+
+        diff_plus, diff_minus, diff_zero = [ [] for _ in range(3) ]
+        cost_plus, cost_minus, cost_zero = [ [] for _ in range(3) ]
+
         for _ in range(M):
             A_time, B_time = (int(t) for t in input().split())
+            diff = A_time - B_time
+            if diff > 0:
+                diff_plus.append(diff)
+                cost_plus.append(A_time)
+            elif diff == 0:
+                diff_zero.append(0)
+                cost_zero.append(A_time)
+            elif diff < 0:
+                diff_minus.append(-diff)
+                cost_minus.append(A_time)
+
+        print(f'[diff] plus: {diff_plus}, zero: {diff_zero}, minus: {diff_minus}')
+        print(f'[cost] plus: {cost_plus}, zero: {cost_zero}, minus: {cost_minus}')
+
+        if len(diff_plus) * len(diff_minus) == 0:
+            print('IMPOSSIBLE')
+            continue
 
 
 if __name__ == '__main__':
--- 2017-06-3W/test_nthlon.py
+++ 2017-06-3W/test_nthlon.py
@@ -2,11 +2,22 @@ from NTHLON import Nthlon
 
 def test_lin_comb():
    nthlon = Nthlon()
+   nthlon.diff_plus = [1, 9]
+   nthlon.cost_plus = [21,10]
    print()
-   nthlon.lin_comb([1,9], [21,10])
+   nthlon.lin_comb(50)
 
 def test_get_answer():
    nthlon = Nthlon()
    print()
-   nthlon.get_answer([1,6,1], [134,31,10], 2)
+   nthlon.diff_minus = [1,6,1]
+   nthlon.cost_minus = [134,31,10]
+   nthlon.get_answer(2)
+   print()
+   nthlon.diff_minus = [1,1,1,1]
+   nthlon.cost_minus = [1,3,5,7]
+   answer = nthlon.get_answer(4)
+   print(answer)
+
+
 
