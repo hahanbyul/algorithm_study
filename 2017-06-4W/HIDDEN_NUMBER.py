2278dd0b6e5713b531f2c4882735475aae6fd937
Hanbyul Kim <hahanbyul@gmail.com>
2017-06-26 16:28:13 +0900

    [FIX] refactring: remove repetative code

--- 2017-06-4W/HIDDEN_NUMBER.py
+++ 2017-06-4W/HIDDEN_NUMBER.py
@@ -2,23 +2,23 @@ class HiddenNumber:
     def __init__(self, problem):
         self.problem = problem
 
-    def isalpha(char):
+    def isalpha(self, char):
         return char.isalpha()
 
-    def isdigit(char):
+    def isdigit(self, char):
         return char.isdigit()
 
-    def find_end_idx_of_number_seq(self, seq, begin):
+    def find_end(self, compare, seq, begin):
         it = begin + 1
-        while it < len(seq) and seq[it].isdigit():
+        while it < len(seq) and compare(seq[it]):
             it += 1
         return it
 
+    def find_end_idx_of_number_seq(self, seq, begin):
+        return self.find_end(self.isdigit, seq, begin)
+
     def find_end_idx_of_alpha_seq(self, seq, begin):
-        it = begin + 1
-        while it < len(seq) and seq[it].isalpha():
-            it += 1
-        return it
+        return self.find_end(self.isalpha, seq, begin)
 
     def solve(self):
         seq = self.problem
@@ -29,9 +29,10 @@ class HiddenNumber:
         while begin < len(seq):
             print(f'begin: {begin}')
             if seq[begin].isalpha():
-                end = self.find_end_idx_of_alpha_seq(seq, begin)
+                end = self.find_end(self.isalpha, seq, begin)
             elif seq[begin].isdigit():
-                end = self.find_end_idx_of_number_seq(seq, begin)
+                end = self.find_end(self.isdigit, seq, begin)
+
                 if end - begin <= 6:
                     hidden_num = int(seq[begin:end])
                     print(hidden_num)
