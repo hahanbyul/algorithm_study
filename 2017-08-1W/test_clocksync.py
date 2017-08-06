40bfb68f8b3ee29b0b0622a5094f91b4bf76a541
Hanbyul Kim <hahanbyul@gmail.com>
2017-08-06 11:14:12 +0900

    [FIX] mat. ver

--- 2017-08-1W/test_clocksync.py
+++ 2017-08-1W/test_clocksync.py
@@ -1,23 +1,73 @@
 from CLOCKSYNC import Clocksync
+import pprint as pp
+import numpy as np
 
-def test_init():
-    cs = Clocksync()
+cs = Clocksync()
 
 def test_push_switch():
-    cs = Clocksync()
     assert cs.push_switch(tuple([12,6,6,6,6,6,12,12,12,12,12,12,12,12,12,12]), 0) == (9,3,3,6,6,6,12,12,12,12,12,12,12,12,12,12)
     assert cs.push_switch(cs.push_switch(tuple([12,6,6,6,6,6,12,12,12,12,12,12,12,12,12,12]), 8), 8) == (12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12)
 
 def test_get_next():
-    cs = Clocksync()
     next_states = cs.get_next(tuple([12,6,6,6,6,6,12,12,12,12,12,12,12,12,12,12]))
     assert len(next_states) == 9
     print(next_states)
 
 def test_ex1():
-    cs = Clocksync()
     assert cs.bfs(tuple([12,6,6,6,6,6,12,12,12,12,12,12,12,12,12,12])) == 2
 
 def test_ex2():
-    cs = Clocksync()
     assert cs.bfs(tuple([int(i) for i in '12 9 3 12 6 6 9 3 12 9 12 9 12 12 6 6'.split()])) == 9
+
+def test_switch_mat():
+    print()
+    switch_mat = cs.get_switch_mat()
+    pp.pprint(switch_mat.T)
+    """
+    switch_mat_inv = np.linalg.inv(cs.get_switch_mat())
+    pp.pprint(switch_mat_inv)
+    pp.pprint(np.dot(switch_mat, switch_mat_inv))
+    """
+
+    asdf = np.array([
+             [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+             [-1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0],
+             [0, -1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+             [-1, -2, 2, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, -1, 0],
+             [0, 0, 0, -1, 0, 0, -1, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, -1, 1, 0, 0, 0, -1, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 1, -1, 0, 1, 0, 0, 0, 0, 0, 0],
+             [1, 0, -1, 0, 0, 0, -1, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+             [1, 0, -1, 0, 0, 1, -1, -1, 2, 1, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, -1, 0, -1, 0],
+             [0, 0, 0, 0, 0, -1, 1, 0, 0, 0, 0, 0, -1, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -1, 1],
+             ])
+
+    print(np.matmul(asdf, switch_mat.T))
+
+    ex1 = np.array([0, 2, 2, 2, 2, 2, 0, 0, 0, 0])
+
+def test_add_multiple_row():
+    mat = [[1, 1], [1, 0]]
+    assert cs.add_multiple_row(mat, 0, 1, -1) == [[1,1], [0,-1]]
+
+def test_identity_mat():
+    print()
+    print(cs.get_identity_mat())
+
+def test_pivotting():
+    cs = Clocksync()
+    for i in range(10):
+        cs.pivotting(cs.switch_mat, i)
+
+def test_swap_rows():
+    mat = [[1, 1], [1, 0]]
+    assert cs.swap_rows(mat, 0, 1) == [[1, 0], [1, 1]]
+
+    mat = [[-1, 1], [1, -1]]
+    assert cs.swap_rows(mat, 0, 1) == [[1, -1], [-1, 1]]
