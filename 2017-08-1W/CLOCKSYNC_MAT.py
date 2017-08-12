5ab91437eccb96a28c48438bf02ab39c51a9c9cf
Hanbyul Kim <hahanbyul@gmail.com>
2017-08-12 14:36:40 +0900

    [FIX] mat ver., initial

--- 2017-08-1W/CLOCKSYNC_MAT.py
+++ 2017-08-1W/CLOCKSYNC_MAT.py
@@ -1,5 +1,4 @@
 # https://algospot.com/judge/problem/read/CLOCKSYNC
-import numpy as np
 
 class Clocksync:
     def __init__(self):
@@ -15,74 +14,43 @@ class Clocksync:
                 9: (3, 4, 5, 9, 13)}
 
         self.answer = tuple([12] * 16)
+        self.switch_mat   = self.get_switch_mat()
+        self.identity_mat = self.get_identity_mat()
 
-        # self.switch_mat = self.get_switch_mat()
-        # self.identity_mat = self.get_identity_mat()
-        # self.pivotted = {}
-
-
-    def bfs(self, problem):
-        visited = {}
-        queue = []
-        queue.append((problem, 0))
-
-        while len(queue) > 0:
-            cur_state, distance = queue.pop(0)
-            # print(f'state: {cur_state}, dist: {distance}')
-            if cur_state == self.answer:
-                return distance
-            if distance > 30:
-                return -1
-
-            # print(f'next: {self.get_next(cur_state)}')
-            for next_state in self.get_next(cur_state):
-                if not visited.get(next_state, False):
-                    queue.append((next_state, distance + 1))
-                    visited[next_state] = True
-            # print(f'q: {queue}')
-
-    def get_next(self, cur_state):
-        next_state_list = []
-        for switch_num, indices in enumerate(self.switch.values()):
-            # if any([cur_state[i] != 12 for i in indices]):
-            next_state = self.push_switch(cur_state, switch_num)
-            next_state_list.append(next_state)
-
-        return next_state_list
-
-    def push_switch(self, cur_state, switch_num):
-        next_state = list(cur_state)
-        for i in self.switch[switch_num]:
-            next_state[i] += 3
-            if next_state[i] == 15:
-                next_state[i] = 3
-
-        return tuple(next_state)
+        for i in range(10):
+            self.pivotting(self.switch_mat, i)
 
     def get_switch_mat(self):
-        mat = []
-        for indices in self.switch.values():
-            mat.append([-1 if i in indices else 0 for i in range(16)])
+        mat = [[0 for _ in range(10)] for _ in range(16)]
+        for col in range(10):
+            for row in self.switch[col]:
+                mat[row][col] = 1
 
         return mat
 
     def get_identity_mat(self):
         return [[0 if i != j else 1 for j in range(16)] for i in range(16)]
 
+    def get_state_mat(self, string):
+        return self.modularize([int(num) // 3 for num in string.split()])
+
+    def modularize(self, array):
+        return [num % 4 for num in array]
+
     @staticmethod
     def add_multiple_row(mat, i, j, scaler):
-        print(f'i: {i}, j: {j}, scaler: {scaler}')
+        # print(f'i: {i}, j: {j}, scaler: {scaler}')
         mat[j] = [e_j + scaler * e_i for e_i, e_j in zip(mat[i], mat[j])]
         return mat
     
     @staticmethod
     def swap_rows(mat, i, j):
-        print(f'mat[i]: {mat[i]}, mat[j]: {mat[j]}')
+        # print(f'mat[i]: {mat[i]}, mat[j]: {mat[j]}')
         mat[i], mat[j] = mat[j], mat[i]
         return mat
 
     def pivotting(self, mat, j):
-        self.print_matrix(mat)
+        # self.print_matrix(mat)
         i = j
         while i < 10 and mat[i][j] == 0:
             i += 1
@@ -95,27 +63,60 @@ class Clocksync:
         i = 0
         while i < 10:
             if i != pivot and mat[i][j] != 0:
-                self.add_multiple_row(mat, pivot, i, int(-mat[i][j]/mat[pivot][j]))
-                self.add_multiple_row(self.identity_mat, pivot, i, int(-mat[i][j]/mat[pivot][j]))
+                scaler = int(-mat[i][j]/mat[pivot][j])
+                self.add_multiple_row(mat, pivot, i, scaler)
+                self.add_multiple_row(self.identity_mat, pivot, i, scaler)
 
             i += 1
 
         self.swap_rows(mat, pivot, j)
         self.swap_rows(self.identity_mat, pivot, j)
-        self.print_matrix(mat)
-        self.print_matrix(self.identity_mat)
+        # self.print_matrix(mat)
+        # self.print_matrix(self.identity_mat)
 
-    @staticmethod
-    def print_matrix(mat):
-        print(f'mat: \n{np.array(mat)}')
+    def solve(self, string):
+        LA = self.switch_mat
+        L = self.identity_mat
+        b = self.get_state_mat(string)
 
+        Lb = []
+        for i in range(16):
+            e = sum([e_iden * (-e_prob) for e_iden, e_prob in zip(L[i], b)])
+            Lb.append(e)
+
+        x = {}
+        x[0] = Lb[0] % 4
+        x[1] = Lb[11] % 4
+        x[2] = Lb[2] % 4
+        x[3] = -Lb[3] % 4
+        x[4] = Lb[4] % 4
+        x[5] = -Lb[5] % 4
+        x[6] = -Lb[6] % 4
+        x[8] = -Lb[8] % 4
+        x[9] = (Lb[9] // 2) % 4
+        x[7] = (Lb[7] - x[9]) % 4
+
+        condition = True
+        condition &= (x[1] - x[9]) % 4 == Lb[1] % 4
+        condition &= (x[2] + x[4]) % 4 == Lb[10] % 4
+        condition &= x[4] == Lb[12] % 4
+        condition &= x[9] == Lb[13] % 4
+        condition &= (x[2] + x[5] + x[6] + x[7]) % 4 == Lb[14] % 4
+        condition &= (x[2] + x[5] + x[6] + x[7]) % 4 == Lb[15] % 4
+
+        if not condition:
+            return -1
+        else:
+            ret = 0
+            for i in range(10):
+                ret += x[i]
+            return ret
 
 def main():
     C = int(input())
     cs = Clocksync()
     for _ in range(C):
-        clocks = tuple([int(i) for i in input().split()])
-        print(cs.bfs(clocks))
+        print(cs.solve(input()))
 
 if __name__ == '__main__':
     main()
--- 2017-08-1W/test_clocksync.py
+++ 2017-08-1W/test_clocksync.py
@@ -1,6 +1,9 @@
 from CLOCKSYNC import Clocksync
+from CLOCKSYNC_MAT import Clocksync as Clocksync_mat
 import pprint as pp
 import numpy as np
+from tkinter import Tk
+import random
 
 cs = Clocksync()
 
@@ -20,50 +23,31 @@ def test_ex2():
     assert cs.bfs(tuple([int(i) for i in '12 9 3 12 6 6 9 3 12 9 12 9 12 12 6 6'.split()])) == 9
 
 def test_switch_mat():
+    cs = Clocksync_mat()
     print()
     switch_mat = cs.get_switch_mat()
-    pp.pprint(switch_mat.T)
-    """
-    switch_mat_inv = np.linalg.inv(cs.get_switch_mat())
-    pp.pprint(switch_mat_inv)
-    pp.pprint(np.dot(switch_mat, switch_mat_inv))
-    """
-
-    asdf = np.array([
-             [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
-             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
-             [-1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0],
-             [0, -1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-             [-1, -2, 2, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, -1, 0],
-             [0, 0, 0, -1, 0, 0, -1, 1, 0, 0, 0, 0, 0, 0, 0, 0],
-             [0, 0, 0, 0, 0, 0, -1, 1, 0, 0, 0, -1, 0, 0, 0, 0],
-             [0, 0, 0, 0, 0, 0, 1, -1, 0, 1, 0, 0, 0, 0, 0, 0],
-             [1, 0, -1, 0, 0, 0, -1, 0, 1, 0, 0, 0, 0, 0, 0, 0],
-             [1, 0, -1, 0, 0, 1, -1, -1, 2, 1, 0, 0, 0, 0, 0, 0],
-             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, -1, 0, -1, 0],
-             [0, 0, 0, 0, 0, -1, 1, 0, 0, 0, 0, 0, -1, 0, 0, 0],
-             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
-             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -1, 1],
-             ])
-
-    print(np.matmul(asdf, switch_mat.T))
-
+    print(np.array(switch_mat))
     ex1 = np.array([0, 2, 2, 2, 2, 2, 0, 0, 0, 0])
 
 def test_add_multiple_row():
+    cs = Clocksync_mat()
     mat = [[1, 1], [1, 0]]
-    assert cs.add_multiple_row(mat, 0, 1, -1) == [[1,1], [0,-1]]
+    answer = np.matmul([[1,0],[-1,1]], mat)
+    cs.add_multiple_row(mat, 0, 1, -1)
+    assert np.array_equal(mat, answer)
 
 def test_identity_mat():
+    cs = Clocksync_mat()
     print()
-    print(cs.get_identity_mat())
+    print(np.array(cs.get_identity_mat()))
 
 def test_pivotting():
-    cs = Clocksync()
+    cs = Clocksync_mat()
     for i in range(10):
         cs.pivotting(cs.switch_mat, i)
+        assert np.array_equal(cs.switch_mat, np.matmul(cs.identity_mat, cs.get_switch_mat()))
+    print(np.array(cs.switch_mat))
+    print(cs.identity_mat)
 
 def test_swap_rows():
     mat = [[1, 1], [1, 0]]
@@ -71,3 +55,45 @@ def test_swap_rows():
 
     mat = [[-1, 1], [1, -1]]
     assert cs.swap_rows(mat, 0, 1) == [[1, -1], [-1, 1]]
+
+def test_get_state_mat():
+    cs = Clocksync_mat()
+    assert cs.get_state_mat('12 6 6 6 6 6 12 12 12 12 12 12 12 12 12 12') == [0, 2, 2, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    assert cs.get_state_mat('12 9 3 12 6 6 9 3 12 9 12 9 12 12 6 6') == [0, 3, 1, 0, 2, 2, 3, 1, 0, 3, 0, 3, 0, 0, 2, 2]
+
+def test_modularize():
+    cs = Clocksync_mat()
+    print(cs.modularize([-5, 1, 2]))
+
+def test_ex1_mat_ver():
+    cs = Clocksync_mat()
+    for i in range(10):
+        cs.pivotting(cs.switch_mat, i)
+    A = np.array(cs.switch_mat)
+    b = -np.array(cs.get_state_mat('12 6 6 6 6 6 12 12 12 12 12 12 12 12 12 12'))
+    print(A)
+    print(np.matmul(cs.identity_mat, b.T))
+
+    b2 = -np.array(cs.get_state_mat('12 9 3 12 6 6 9 3 12 9 12 9 12 12 6 6'))
+    print(np.matmul(cs.identity_mat, b2.T))
+
+def test_solve():
+    cs = Clocksync_mat()
+    assert cs.solve('12 6 6 6 6 6 12 12 12 12 12 12 12 12 12 12') == 2
+
+def test_solve_ex2():
+    cs = Clocksync_mat()
+    assert cs.solve('12 9 3 12 6 6 9 3 12 9 12 9 12 12 6 6') == 9
+
+def test_compare():
+    cs1 = Clocksync()
+    cs2 = Clocksync_mat()
+    while True:
+        problem = ' '.join([str(random.randint(1, 4)*3) for _ in range(16)])
+        print(problem)
+        answer_mat = cs2.solve(problem)
+        if answer_mat != -1:
+            print(answer_mat)
+            answer = cs.solve(problem)
+            assert answer == answer_mat
+            break
