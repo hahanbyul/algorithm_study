71dc006e8ebc670ec688f16fcff7a26834a4315d
Hanbyul Kim <hahanbyul@gmail.com>
2017-08-26 12:45:46 +0900

    [FIX] LIS: recon impl

--- 2017-07-4W/LIS.py
+++ 2017-07-4W/LIS.py
@@ -1,16 +1,13 @@
 class LIS:
     def __init__(self):
         self.cache = {}
+        self.choices = {}
 
     def read_seq(self, string):
         self.seq = [int(i) for i in string.split()]
 
     def solve(self):
-        ret = 0
-        for i in range(len(self.seq)):
-            ret = max(ret, self.lis(i))
-
-        return ret
+        return self.lis(-1)-1
 
     def lis(self, start):
         if self.cache.get(start, False):
@@ -18,11 +15,24 @@ class LIS:
 
         ret = 1
         for i in range(start+1, len(self.seq)):
-            if self.seq[i] > self.seq[start]:
-                ret = max(ret, 1 + self.lis(i))
+            if start == -1 or self.seq[i] > self.seq[start]:
+                ans = 1 + self.lis(i)
+                if ret < ans:
+                    ret = ans
+                    self.choices[start] = i
 
         self.cache[start] = ret
         return ret
+
+    def reconstruct(self):
+        return self.reconstruct_(self.choices[-1], [])
+
+    def reconstruct_(self, start, seq):
+        print("start: %d, seq: %s" % (start, seq))
+        seq.append(self.seq[start])
+        if start not in self.choices.keys():
+            return seq
+        return self.reconstruct_(self.choices[start], seq)
 
 
 def main():
--- 2017-07-4W/test_lis.py
+++ 2017-07-4W/test_lis.py
@@ -17,22 +17,30 @@ def test_solve():
     lis.seq = [5,4,3,2,1,6,7,8]
     assert lis.solve() == 4
     print(lis.cache)
+    print(lis.choices)
+    print(lis.reconstruct())
     
 def test_ex3():
     lis = LIS()
     lis.seq = [5,6,7,8,1,2,3,4]
     assert lis.solve() == 4
     print(lis.cache)
+    print(lis.choices)
+    print(lis.reconstruct())
 
 def test_ex4():
     lis = LIS()
     lis.seq = [9, 1, 3, 7, 5, 6, 20]
     lis.solve() == 5
     print(lis.cache)
+    print(lis.choices)
+    print(lis.reconstruct())
 
 def test_ex5():
     lis = LIS()
     lis.seq = [1, 3, 2, 5, 3, 4, 5]
     lis.solve() == 5
     print(lis.cache)
+    print(lis.choices)
+    print(lis.reconstruct())
 
