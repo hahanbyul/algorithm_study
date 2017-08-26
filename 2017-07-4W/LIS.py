8409c1de1bbf20277b69dd752dc3cf9eb8cec5c4
Hanbyul Kim <hahanbyul@gmail.com>
2017-08-26 12:03:55 +0900

    [FIX] LIS: online -> PASS

--- 2017-07-4W/LIS.py
+++ 2017-07-4W/LIS.py
@@ -1,34 +1,28 @@
 class LIS:
     def __init__(self):
         self.cache = {}
-        self.max_val = 0
 
-    def solve(self, seq):
-        for i, num in enumerate(seq):
-            ret = self.cache.get(i, False)
-            if not ret:
-                ret = self.max_length(seq[i:], 0, i)
-                self.cache[i] = ret
-            self.max_val = max(self.max_val, ret)
+    def read_seq(self, string):
+        self.seq = [int(i) for i in string.split()]
 
-        print(self.max_val)
-        return self.max_val
+    def solve(self):
+        ret = 0
+        for i in range(len(self.seq)):
+            ret = max(ret, self.lis(i))
 
-    def max_length(self, seq, num, index):
-        if self.cache.get(index, False):
-            return self.cache[index]
+        return ret
 
-        if len(seq) == 0:
-            return 0
+    def lis(self, start):
+        if self.cache.get(start, False):
+            return self.cache[start]
 
-        if seq[0] <= num:
-            return self.max_length(seq[1:], num, index+1)
-        else:
-            ret = 0
-            for i in range(len(seq)):
-                ret = max(ret, 1 + self.max_length(seq[i+1:], seq[0], index+i+1))
-            self.cache[index] = ret
-            return ret
+        ret = 1
+        for i in range(start+1, len(self.seq)):
+            if self.seq[i] > self.seq[start]:
+                ret = max(ret, 1 + self.lis(i))
+
+        self.cache[start] = ret
+        return ret
 
 
 def main():
@@ -36,9 +30,8 @@ def main():
     for _ in range(C):
         N = int(input())
         lis = LIS()
-
-        seq = [int(i) for i in input().split()]
-        lis.solve(seq)
+        lis.read_seq(input())
+        print(lis.solve())
 
 
 if __name__ == '__main__':
--- 2017-07-4W/test_lis.py
+++ 2017-07-4W/test_lis.py
@@ -1,4 +1,4 @@
-from LIS import LIS
+from lis import LIS
 
 def test_ex1():
     lis = LIS()
@@ -14,21 +14,25 @@ def test_ex2():
 
 def test_solve():
     lis = LIS()
-    assert lis.solve([5,4,3,2,1,6,7,8]) == 4
+    lis.seq = [5,4,3,2,1,6,7,8]
+    assert lis.solve() == 4
     print(lis.cache)
     
 def test_ex3():
     lis = LIS()
-    assert lis.solve([5,6,7,8,1,2,3,4]) == 4
+    lis.seq = [5,6,7,8,1,2,3,4]
+    assert lis.solve() == 4
     print(lis.cache)
 
 def test_ex4():
     lis = LIS()
-    lis.solve([9, 1, 3, 7, 5, 6, 20]) == 5
+    lis.seq = [9, 1, 3, 7, 5, 6, 20]
+    lis.solve() == 5
     print(lis.cache)
 
 def test_ex5():
     lis = LIS()
-    lis.solve([1, 3, 2, 5, 3, 4, 5]) == 5
+    lis.seq = [1, 3, 2, 5, 3, 4, 5]
+    lis.solve() == 5
     print(lis.cache)
 
