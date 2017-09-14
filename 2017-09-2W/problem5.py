26bdfc13e488e465a73bbc2dd5d67466878b255d
Hanbyul Kim <hahanbyul@gmail.com>
2017-09-14 20:22:20 +0900

    [FIX] problem5: verification

--- 2017-09-2W/problem5.py
+++ 2017-09-2W/problem5.py
@@ -1,4 +1,17 @@
-def solve(land, row_index, prev_i):
+def dfs(land, picked, row_index, prev_i):
+    if row_index >= len(land):
+        print(picked)
+        return
+
+    for col_index, x in enumerate(land[row_index]):
+        if prev_i == col_index:
+            continue
+        picked.append(x)
+        dfs(land, picked, row_index+1, col_index)
+        picked.pop()
+
+def solve(land, picked, row_index, prev_i):
+    print(picked)
     if row_index >= len(land):
         return 0
 
@@ -6,10 +19,12 @@ def solve(land, row_index, prev_i):
     for index, x in enumerate(land[row_index]):
         if index == prev_i:
             continue
-        ret = max(ret, x + solve(land, row_index+1, index))
+        picked.append(x)
+        ret = max(ret, x + solve(land, picked, row_index+1, index))
+        picked.pop()
 
     return ret
 
 def solution(land):
-    answer = solve(land, 0, -1)
+    answer = solve(land, [], 0, -1)
     return answer
