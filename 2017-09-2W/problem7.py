a704f9cf812be19febed545258f0ed785b0d23b8
Hanbyul Kim <hahanbyul@gmail.com>
2017-09-16 00:08:10 +0900

    [FIX] problem7: cache

--- 2017-09-2W/problem7.py
+++ 2017-09-2W/problem7.py
@@ -1,31 +1,34 @@
-def solve(strs, t, start):
+def solve(cache, strs, t, start):
     if len(t) == 0:
         return 0
 
+    if cache.get(start, False):
+        return cache[start]
+
     ret = 20000
     for piece in strs:
-        print(f'word: {piece}')
         if piece[0] < t[0]:
             continue
         if piece[0] > t[0]:
             break
         
         if piece == t[:len(piece)]:
-            remainder = solve(strs, t[len(piece):], start + len(piece))
+            remainder = solve(cache, strs, t[len(piece):], start + len(piece))
             if remainder == -1:
                 continue
             ret = min(ret, 1 + remainder)
 
-    print(f't: {t}, ret: {ret}')
 
     if ret == 20000:
         return -1
 
+    cache[start] = ret
     return ret
 
 def solution(strs, t):
     strs.sort()
-    print(f'strs: {strs}')
-    answer = solve(strs, t, 0)
+
+    cache = {}
+    answer = solve(cache, strs, t, 0)
 
     return answer
