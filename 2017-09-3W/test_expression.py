6144670d89874942ad8238098c1c10582fcc3c72
Hanbyul Kim <hahanbyul@gmail.com>
2017-09-22 03:46:30 +0900

    [FIX] EXPRESSION: func. ver., online -> PASS

--- 2017-09-3W/test_expression.py
+++ 2017-09-3W/test_expression.py
@@ -1,15 +1,17 @@
-from expression import Expression
+from expression import move_right
+from expression import get_initial_array
+from expression import expressions
 
 def test_move_right():
     array = [2, 3, 4]
-    assert Expression.move_right(array, 9) == 12 and array == [3, 4, 5]
-    assert Expression.move_right(array, 12) == 15 and array == [4, 5, 6]
+    assert move_right(array, 9) == 12 and array == [3, 4, 5]
+    assert move_right(array, 12) == 15 and array == [4, 5, 6]
 
 def test_get_initial_array():
-    assert Expression.get_initial_array(15) == ([1, 2, 3, 4, 5], 15)
-    assert Expression.get_initial_array(20) == ([1, 2, 3, 4, 5, 6], 21)
+    assert get_initial_array(15) == ([1, 2, 3, 4, 5], 15)
+    assert get_initial_array(20) == ([1, 2, 3, 4, 5, 6], 21)
 
 def test_ex():
-    ex = Expression()
-    assert ex.solve(15) == 4
+    # assert expressions(15) == 4
+    assert expressions(30)
 
