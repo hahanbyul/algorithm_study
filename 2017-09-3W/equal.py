76f8bd5fa1b572e6380698581b9bc59f1953a104
Hanbyul Kim <hahanbyul@gmail.com>
2017-09-22 01:22:58 +0900

    [FIX] EQUAL: online score -> 19.5

--- 2017-09-3W/equal.py
+++ 2017-09-3W/equal.py
@@ -1,6 +1,3 @@
-import math
-import heapq
-
 class Equal:
     def __init__(self, initial):
         self.array = initial
@@ -13,7 +10,18 @@ class Equal:
         return array
 
     @staticmethod
+    def inc_1_except_0(array):
+        for i in range(len(array)):
+            if array[i] == 0:
+                continue
+            array[i] += 1
+        return array
+
+    @staticmethod
     def compute_add_times(candy):
+        if candy == 0:
+            return 0
+
         add_5  = candy // 5
         remain = candy %  5
         add_2  = remain // 2
@@ -21,14 +29,22 @@ class Equal:
 
         return add_5 + add_2 + add_1
 
-    def solve(self):
-        self.sub_min(self.array)
-
+    def sum_count(self, array):
         ret = 0
         for x in self.array:
             ret += self.compute_add_times(x)
 
         return ret
+
+    def solve(self):
+        array = self.sub_min(self.array)
+        answer = self.sum_count(array)
+
+        for i in range(1, 5):
+            self.inc_1_except_0(array)
+            answer = min(answer, self.compute_add_times(i) + self.sum_count(array))
+
+        return answer
 
 def main():
     T = int(input())
@@ -37,7 +53,7 @@ def main():
         initial = [int(x) for x in input().split()]
 
         eq = Equal(initial)
-        print(eq.solve_bfs())
+        print(eq.solve())
 
 if __name__ == '__main__':
     main()
