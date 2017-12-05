03e3a1923d549b9e7f12f986ad1478fe1434d207
Hanbyul Kim <hahanbyul@gmail.com>
2017-12-05 16:24:05 +0900

    [ADD] RAL: solve, ex -> PASS

--- 2017-11-4W/roads_and_libraries.py
+++ 2017-11-4W/roads_and_libraries.py
@@ -2,9 +2,27 @@
 # problem url: https://www.hackerrank.com/challenges/torque-and-development/problem
 
 class RoadsAndLibraries():
+    def read_input_file(self, filename):
+        f = open(filename, 'r')
+        q = int(f.readline().strip())
+        for _ in range(q):
+            n, m, x, y = f.readline().strip().split(' ')
+            self.num_city, self.num_road, self.cost_lib, self.cost_road = [int(n), int(m), int(x), int(y)]
+
+            # initialize graph
+            self.library = [False for _ in range(self.num_city)]
+            self.road    = [[False for _ in range(self.num_city)] for _ in range(self.num_city)]
+            
+            for a1 in range(self.num_road):
+                city_1, city_2 = f.readline().strip().split(' ')
+                city_1, city_2 = [int(city_1), int(city_2)]
+
+                self.road[city_1-1][city_2-1] = True
+                self.road[city_2-1][city_1-1] = True
+
     def read_input(self):
-        case_num = int(input().strip())
-        for _ in range(case_num):
+        q = int(input().strip())
+        for _ in range(q):
             n, m, x, y = input().strip().split(' ')
             self.num_city, self.num_road, self.cost_lib, self.cost_road = [int(n), int(m), int(x), int(y)]
 
@@ -23,11 +41,41 @@ class RoadsAndLibraries():
         if self.cost_lib < self.cost_road:
             return self.num_city * self.cost_lib
 
-        for num_lib in range(1, self.num_city+1):
+        for num_lib in range(1, self.num_city):
             num_road = self.num_city - num_lib
 
-            if is_possible_with_these(num_lib, num_road):
-                return num_lib * cost_lib + num_road * cost_road
+            visited = [False for _ in range(self.num_city)]
+            if self.is_possible_with_these(num_lib, num_road, visited):
+                return num_lib * self.cost_lib + num_road * self.cost_road
 
-    def is_possible_with_these(self, num_lib, num_road):
-        return
+        # if num_lib == num_city (not returned before)
+        return self.num_city * self.cost_lib
+
+    def is_possible_with_these(self, num_lib, num_road, visited):
+        if num_road <= 0:
+            return True if num_lib + num_road == 0 and all(visited) else False
+        if num_lib == 0 and num_road == 0:
+            return True if all(visited) else False
+
+        for i, is_visited in enumerate(visited):
+            if is_visited:
+                continue
+
+            remained_road = num_road - self.compute_how_many_cities_are_connected(i, visited)
+            return self.is_possible_with_these(num_lib-1, remained_road, visited)
+
+    def compute_how_many_cities_are_connected(self, city, visited):
+        visited[city] = True
+
+        sum_cities = 0
+        for next_city, is_road in enumerate(self.road[city]):
+            if not is_road or visited[next_city]:
+                continue
+
+            sum_cities += 1
+            self.road[city][next_city] = False
+            self.road[next_city][city] = False
+
+            sum_cities += self.compute_how_many_cities_are_connected(next_city, visited)
+
+        return sum_cities
--- 2017-11-4W/test_roads_and_libraries.py
+++ 2017-11-4W/test_roads_and_libraries.py
@@ -4,3 +4,40 @@ ral = RoadsAndLibraries()
 
 def test_read_input():
     ral.read_input()
+
+def test_true_plus():
+    assert True + True == 2
+
+def test_all_true():
+    assert all([True, True])
+    assert not all([False, True])
+
+def test_compute_how_many_ex1():
+    for i in [0, 1, 2]:
+        ex = RoadsAndLibraries()
+        ex.read_input_file('ex1.txt')
+        visited = [False for _ in range(ex.num_city)]
+        assert ex.compute_how_many_cities_are_connected(i, visited) == 2
+
+def test_compute_how_many_ex2():
+    for i in [0, 1, 2, 3]:
+        ex = RoadsAndLibraries()
+        ex.read_input_file('ex2.txt')
+        visited = [False for _ in range(ex.num_city)]
+        assert ex.compute_how_many_cities_are_connected(i, visited) == 3
+
+    for i in [4, 5]:
+        ex = RoadsAndLibraries()
+        ex.read_input_file('ex2.txt')
+        visited = [False for _ in range(ex.num_city)]
+        assert ex.compute_how_many_cities_are_connected(i, visited) == 1
+
+def test_solve_ex1():
+    ex = RoadsAndLibraries()
+    ex.read_input_file('ex1.txt')
+    assert ex.solve() == 4
+
+def test_solve_ex2():
+    ex = RoadsAndLibraries()
+    ex.read_input_file('ex2.txt')
+    assert ex.solve() == 12
