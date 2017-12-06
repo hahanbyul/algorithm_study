0e163e15c8965d6d7af8f60299dd83c828b7d784
Hanbyul Kim <hahanbyul@gmail.com>
2017-12-06 14:49:10 +0900

    [FIX] bit_change: zero_to_carry array, ex -> PASS

--- 2017-11-4W/bit_change.py
+++ 2017-11-4W/bit_change.py
@@ -1,17 +1,13 @@
-def compute_zero_to_next_carry(N):
-    array = [0 for _ in range(N+1)]
-    for i in range(2, N+1):
-        array[i] = array[i-1] + i
-
-    return array
-
-def compute_summed_array(array):
-    summed_array = [0 for _ in range(len(array))]
-    summed_array[0] = array[0]
-    for i in range(1, len(summed_array)):
+def compute_array_advance(N):
+    array        = [0 for _ in range(N+1)]
+    summed_array = [0 for _ in range(N+1)]
+    array[2]        = 2
+    summed_array[2] = 2
+    for i in range(3, N+1):
+        array[i]        = summed_array[i-1] + i
         summed_array[i] = summed_array[i-1] + array[i]
 
-    return summed_array
+    return array, summed_array
 
 def count_zero(string):
     return count_cont_char(string, '0')
@@ -33,25 +29,43 @@ def compute_score(string, array):
 
     return score
 
+def next_change(x):
+    return 1 if x[-1] == '0' else count_one(x) + 1
+
+ref_cache = {}
+def bit_change_ref(x, y):
+    if y <= 1: return 0
+    if ref_cache.get((x-1, y)):
+        return ref_cache[(x-1, y)] + next_change(x-1)
+
+    summed = 0
+    for num in range(x, y):
+        summed += next_change(bin(num))
+    ret = summed
+    ref_cache[(x,y)] = ret
+
+    return ret
+
 def compute_odd_num(x, y):
     num = y - x + 1
     odd_num = num // 2 - 1 if x % 2 == 1 and y % 2 == 0 else num // 2
     return odd_num
 
 def bit_change(x, y):
-    zero_to_next_carry = compute_zero_to_next_carry(len(bin(y - x)))
-    zero_to_next_carry_summed = compute_summed_array(zero_to_next_carry)
+    zero_to_next_carry, zero_to_next_carry_summed = compute_array_advance(len(bin(y - x)))
 
     current = x
     summed = compute_odd_num(x, y)
-    print('odd:',summed)
+    # print('odd:',summed)
 
     while y - current > 0:
+        """
         diff = y - current
         print('current: %d,' % current, bin(current))
         print('y: %d,' % y, bin(y))
         print('diff: %d,' % diff, bin(diff))
         print('summed:',summed)
+        """
         zero_num = count_zero(bin(current))
         if zero_num == 0:
             one_num = count_one(bin(current))
@@ -60,9 +74,7 @@ def bit_change(x, y):
             continue
 
         if zero_num == 1:
-            one_num = count_one(bin(current+1))
-            summed += one_num + 1
-            current += 2
+            current += 1
             continue
         
         diff = y - current
@@ -82,4 +94,5 @@ def bit_change(x, y):
             summed += compute_score(bin(diff), zero_to_next_carry)
             break
  
+    # print("==> answer:", summed)
     return summed
--- 2017-11-4W/test_bit_change.py
+++ 2017-11-4W/test_bit_change.py
@@ -1,18 +1,30 @@
-from bit_change import compute_zero_to_next_carry
-from bit_change import compute_summed_array
+from bit_change import compute_array_advance
 from bit_change import count_zero
+from bit_change import count_one
 from bit_change import compute_score
 from bit_change import compute_odd_num
 from bit_change import bit_change
+from bit_change import bit_change_ref
+from bit_change import next_change
 
 def test_bit_format():
     print('{0:b}'.format(38))
     print(bin(38)+bin(1))
 
 def test_cache():
-    arr = compute_zero_to_next_carry(40)
-    print(arr)
-    print(compute_summed_array(arr))
+    M = 10
+    arr, _ = compute_array_advance(M)
+
+    arr2 = [0 for _ in range(M+1)]
+    for i in range(M+1):
+        if i <= 1:
+            arr2[i] = 0
+            continue
+
+        upper = 2 ** (i-1)
+        arr2[i] = bit_change_ref(0, upper) - compute_odd_num(0, upper)
+
+    assert arr == arr2
 
 def test_count_zero():
     assert count_zero('1000') == 3
@@ -20,8 +32,13 @@ def test_count_zero():
     assert count_zero('10010010') == 1
     assert count_zero('00') == 2
 
+def test_count_one():
+    assert count_one('1111') == 4
+    assert count_one('111111') == 6
+    assert count_one('1000') == 0
+
 def test_compute_score():
-    arr = compute_zero_to_next_carry(20)
+    arr = compute_array_advance(20)
     string = '1111'
     assert compute_score(string, arr) == 16
 
@@ -38,8 +55,23 @@ def test_odd_num():
     assert compute_odd_num(10, 15) == 3
     assert compute_odd_num(18, 25) == 4
 
-def test_bit_change():
-    assert bit_change(11, 15) == 7
-    assert bit_change(11, 16) == 12
-    assert bit_change(22, 25) == 6
-    # assert bit_change(220687542, 1053328179) == 1665281275
+def test_bit_change_ref():
+    assert bit_change_ref(11, 15) == 7
+    assert bit_change_ref(11, 16) == 12
+    assert bit_change_ref(22, 25) == 6
+    # assert bit_change_ref(220687542, 1053328179) == 1665281275
+
+def test_bit_change_for_examples():
+    assert bit_change(11, 15) == bit_change_ref(11, 15)
+    assert bit_change(22, 25) == bit_change_ref(22, 25)
+    assert bit_change(220687542, 1053328179) == 1665281275
+
+def test_bit_change_for_range():
+    for i in range(11, 100):
+        print('================> (10, %d) <=================' % i)
+        assert bit_change(10, i) == bit_change_ref(10, i)
+
+def test_next_change():
+    assert next_change('0') == 1
+    assert next_change('1') == 2
+    assert next_change('11111') == 6
