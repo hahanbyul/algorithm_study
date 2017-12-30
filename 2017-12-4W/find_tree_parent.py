50ce56632b93683ca9fdcf1831f754282eb59759
Hanbyul Kim <hahanbyul@gmail.com>
2017-12-30 17:24:39 +0900

    [ADD] find_tree_parent: online -> RTE

---
+++ 2017-12-4W/find_tree_parent.py
@@ -0,0 +1,40 @@
+# url: https://www.acmicpc.net/problem/11725
+
+class FindTreeParent:
+    def read_input(self):
+        N = int(input())
+        self.N = N
+        self.tree = [[] for _ in range(N+1)]
+
+        for _ in range(N-1):
+            one, the_other = [int(x) for x in input().split()]
+            self.tree[one].append(the_other)
+            self.tree[the_other].append(one)
+
+    def solve(self):
+        self.visited = [False for _ in range(self.N+1)]
+        self.parent = {}
+        self.find_children(1)
+
+        for node in range(2, self.N+1):
+            print(self.parent[node])
+
+    def find_children(self, node):
+        self.visited[node] = True
+        childeren = self.tree[node]
+
+        for child in childeren:
+            if self.visited[child]:
+                continue
+
+            self.parent[child] = node
+            self.find_children(child)
+
+
+def main():
+    ftp = FindTreeParent()
+    ftp.read_input()
+    ftp.solve()
+
+if __name__ == '__main__':
+    main()
