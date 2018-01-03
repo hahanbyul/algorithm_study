eeb410edf772dbc07cfaf5f7d67ae900acba9ccd
Hanbyul Kim <hahanbyul@gmail.com>
2018-01-03 16:50:02 +0900

    [ADD] construct_tree(), count_subtree()

--- 2018-01-1W/decomposition_reaction_dp.py
+++ 2018-01-1W/decomposition_reaction_dp.py
@@ -3,31 +3,47 @@
 class DecomReaction:
     def read_input(self, input_method):
         self.N, self.M = [int(x) for x in input_method().split()]
-        self.connection = [[] for _ in range(self.N)]
+        self.graph = [[] for _ in range(self.N)]
+        self.tree  = [[] for _ in range(self.N)]
         self.vertex_count = [0 for _ in range(self.N)]
         self.subtree_count = [1 for _ in range(self.N)]
 
         for i in range(self.N-1):
             left_atom, right_atom = [int(x)-1 for x in input_method().split()]
 
-            self.connection[left_atom].append(right_atom)
-            self.connection[right_atom].append(left_atom)
+            self.graph[left_atom].append(right_atom)
+            self.graph[right_atom].append(left_atom)
 
             self.vertex_count[left_atom]  += 1
             self.vertex_count[right_atom] += 1
 
-    def count_subtree(self, node, visited):
-        visited[node] = True
+    def construct_tree(self):
+        visited = [False for _ in range(self.N)]
+        self.root = 0
+        self.add_child(self.root, visited)
 
-        children = self.connection[node]
+    def add_child(self, node, visited):
+        visited[node] = True
+        children = self.graph[node]
+
         for child in children:
             if visited[child]:
                 continue
 
-            self.subtree_count[node] += self.count_subtree(child, visited)
-            visited[child] = False
+            self.tree[node].append(child)
+            self.add_child(child, visited)
+
+    def count_subtree(self, node=None):
+        if node is None:
+            node = self.root
+
+        children = self.tree[node]
+        for child in children:
+            self.subtree_count[node] += self.count_subtree(child)
 
         return self.subtree_count[node]
+
+    """
 
     def solve(self, root, node, cut, visited):
         visited[node] = True
@@ -39,7 +55,7 @@ class DecomReaction:
         elif diff < cut:
             return self.min_cut[root][diff]
 
-        children = self.connection[node]
+        children = self.graph[node]
         ret = cut
         for child in children:
             if visited[child]:
@@ -70,19 +86,34 @@ class DecomReaction:
             ret = min(ret, self.solve_div(root, children, start+1, cut - ccc, summed + ccc, upper_bound))
 
 
-    def all_comb(self, start, children_num, goal, summed, picked):
+    def all_comb(self, start, children_num, goal, summed, picked, children=None, answer=None, root, root_cut):
         if start == children_num-1:
-            picked.append(goal-summed)
-            print(picked)
-            picked.pop()
-            return
+            ret = self.min_cut[root][root_cut]
+            self.min_cut[root][root_cut] = min(ret, answer + self.solve(children[start], goal-summed))
 
         for num in range(goal+1):
             if summed + num > goal:
                 continue
 
             picked.append(num)
-            self.all_comb(start+1, children_num, goal, summed + num, picked)
+            self.all_comb(start+1, children_num, goal, summed + num, picked, children, answer + self.solve(children[start], num), root, root_cut)
             picked.pop()
 
     def solve(self, node, cut, visited):
+        if cut <= 1:
+            return cut
+        count = self.subtree_count[node]
+        diff = count - cut
+        if diff < 0:
+            return float('inf')
+        if 0 <= diff <= 1:
+            return 1
+        elif diff < cut:
+            return self.solve(node, diff, visited)
+
+        ret = cut
+        children = self.tree[node]
+        self.all_comb(0, len(children), cut, 0, [], children, 0)
+        return self.min_cut[node][cut]
+
+"""
--- 2018-01-1W/test_decom_reaction.py
+++ 2018-01-1W/test_decom_reaction.py
@@ -4,3 +4,16 @@ def test_all_comb():
     dr = DecomReaction()
     # dr.all_comb(start=0, children_num=4, goal=5, summed=0, picked=[])
     dr.all_comb(start=0, children_num=1, goal=5, summed=0, picked=[])
+
+def test_construct_tree():
+    dr = DecomReaction()
+    with open('ex_7_2.txt') as f:
+        dr.read_input(f.readline)
+
+    dr.construct_tree()
+    print(dr.tree)
+
+    dr.count_subtree()
+    print(dr.subtree_count)
+
+
