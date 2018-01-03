5a9ef781c4366c5aa14e45379da562e272949f9b
Hanbyul Kim <hahanbyul@gmail.com>
2018-01-04 01:17:03 +0900

    [FIX] decom: new idea, print ver.

--- 2018-01-1W/decomposition_reaction_dp.py
+++ 2018-01-1W/decomposition_reaction_dp.py
@@ -6,8 +6,9 @@ class DecomReaction:
         self.graph = [[] for _ in range(self.N)]
         self.tree  = [[] for _ in range(self.N)]
         self.subtree_count = [1 for _ in range(self.N)]
-        self.min_cut = [[float('inf') for _ in range(self.N)] for _ in range(self.N)]
-        self.visited = [[False for _ in range(self.N)] for _ in range(self.N)]
+
+        self.min_cut_with_root = [[float('inf') for _ in range(self.N)] for _ in range(self.N)]
+        self.min_cut_without_root = [[float('inf') for _ in range(self.N)] for _ in range(self.N)]
 
         for i in range(self.N-1):
             left_atom, right_atom = [int(x)-1 for x in input_method().split()]
@@ -41,92 +42,54 @@ class DecomReaction:
 
         return self.subtree_count[node]
 
-    """
-
-    def solve(self, root, node, cut, visited):
-        visited[node] = True
-
-        count = self.subtree_count[node]
-        diff = count - cut
-        if 0 <= diff <= 1:
-            return 1
-        elif diff < cut:
-            return self.min_cut[root][diff]
-
-        children = self.graph[node]
-        ret = cut
-        for child in children:
-            if visited[child]:
-                continue
-            if self.subtree_count[child] < cut:
-                continue
-            ret = min(ret, self.solve(root, child, cut, visited))
-
-        if ret <= 2:
-            return 
-
-        self.solve_div(root, children, cut, 0, upper_bound)
-
-    def solve_div(self, root, children, start, cut, summed, upper_bound):
-        if start == len(children) and cut > 0:
-            return float('inf')
-        if upper_bound <= summed:
+    def solve_without_root(self, node, cut):
+        print(f'without_root ({node}, {cut})')
+        count = self.subtree_count[node] 
+        if count <= cut:
+            print('answer: inf')
             return float('inf')
 
-        child = children[start]
-        
-        ret = upper_bound
-        for c in range(2, cut-1):
-            ccc = self.min_cut[node][c]
-            if ccc >= upper_bound-1:
-                continue
-
-            ret = min(ret, self.solve_div(root, children, start+1, cut - ccc, summed + ccc, upper_bound))
-
-
-"""
-
-    def solve(self, node, cut):
-        print(f'node: {node}, cut: {cut}')
-        if self.visited[node][cut]:
-            print('cached!!')
-            return self.min_cut[node][cut]
-
-        self.visited[node][cut] = True
-
-        count = self.subtree_count[node]
-        if cut == 0:
-            return 0
-        if node <= self.N-1 and cut == 1:
-            return 1
-
-        if count < cut:
-            return float('inf')
-        if count == cut:
-            return 1
-
-        self.solve_all_combination(0, count - cut, 0, 0, node, cut)
-        ret = self.min_cut[node][cut] + 1
+        ret = float('inf')
         children = self.tree[node]
         for child in children:
-            new_answer = self.solve(child, cut)
-            print(f'new_answer: {new_answer}')
-            ret = min(ret, new_answer)
+            ret = min(ret, 1 + self.solve_with_root(child, cut))
+            ret = min(ret, self.solve_without_root(child, cut))
 
-        self.min_cut[node][cut] = ret
+        print(f'answer: {ret}')
         return ret
 
-    def solve_all_combination(self, start, goal, summed, answer, root, root_cut):
+    def solve_with_root(self, node, cut):
+        print(f'with_root ({node}, {cut})')
+        children = self.tree[node]
+        if cut == 0:
+            print('answer: 1')
+            return 1
+        if cut == 1:
+            print(f'answer: {len(children)}')
+            return len(children)
+        count = self.subtree_count[node] 
+        if count < cut:
+            print('answer: inf')
+            return float('inf')
+        if count == cut:
+            print('answer: 0')
+            return 0
+
+        self.solve_all_combination(0, cut-1, 0, 0, node, cut)
+        print(f'answer: {self.min_cut_with_root[node][cut]}')
+        return self.min_cut_with_root[node][cut]
+
+    def solve_all_combination(self, start, goal, progress, answer, root, cut):
         children = self.tree[root]
         if start == len(children)-1:
-            old_answer = self.min_cut[root][root_cut]
-            new_answer = answer + self.solve(children[start], goal-summed)
-            print(f'update ({root}, {root_cut}): {new_answer}')
-            self.min_cut[root][root_cut] = min(old_answer, new_answer)
+            old_answer = self.min_cut_with_root[root][cut]
+            new_answer = answer + self.solve_with_root(children[start], goal - progress)
+            print(f'sum: {new_answer}')
+            self.min_cut_with_root[root][cut] = min(old_answer, new_answer)
             return
 
-        for num in range(goal+1):
-            if summed + num > goal:
+        for step in range(goal+1):
+            if progress + step > goal:
                 continue
-
-            self.solve_all_combination(start+1, goal, summed + num, answer + self.solve(children[start], num), root, root_cut)
+            delta = self.solve_with_root(children[start], step)
+            self.solve_all_combination(start+1, goal, progress + step, answer + delta, root, cut)
--- 2018-01-1W/test_decom_reaction.py
+++ 2018-01-1W/test_decom_reaction.py
@@ -8,44 +8,26 @@ def test_all_comb():
 dr = DecomReaction()
 with open('ex_7_2.txt') as f:
     dr.read_input(f.readline)
-
-def test_construct_tree():
-    dr.construct_tree()
-    print(dr.tree)
-
-    dr.count_subtree()
-    print(dr.subtree_count)
+dr.construct_tree()
+dr.count_subtree()
+print(dr.tree)
+print(dr.subtree_count)
 
 def test_solve_trivial_cases():
-    dr.construct_tree()
-    dr.count_subtree()
-    print(dr.tree)
-    print(dr.subtree_count)
+    assert dr.solve_with_root(1, 0) == 1
+    assert dr.solve_with_root(2, 1) == 2
 
-    # cut <= 1 
-    assert dr.solve(3,1) == 1
+    assert dr.solve_without_root(1, 3) == float('inf')
+    assert dr.solve_without_root(2, 3) == float('inf')
 
-    # tree_size < cut
-    assert dr.solve(1,4) == float('inf')
+def test_solve():
+    # assert dr.solve_without_root(0, 2) == 2
+    # assert dr.solve_with_root(0, 2) == 3
 
-    # tree_size == cut
-    assert dr.solve(1,3) == 1
-    assert dr.solve(2,3) == 1
+    # assert dr.solve_without_root(0, 3) == 1
+    # assert dr.solve_with_root(0, 3) == 2
 
-    # tree_size == cut-1
-    assert dr.solve(1,2) == 1
-
-def test_solve_trivial_cases():
-    dr.construct_tree()
-    dr.count_subtree()
-    print(dr.tree)
-    print(dr.subtree_count)
-
-    # assert dr.solve(0, 1) == 1
-
-    # assert dr.solve(1, 2) == 2
-    # assert dr.solve(2, 3) == 1
-    assert dr.solve(0, 2) == 2
-
-    # assert dr.solve(0, 4) == 1
-    # assert dr.solve(0, 5) == 2
+    assert dr.solve_with_root(0, 4) == 1
+    assert dr.solve_with_root(0, 5) == 2
+    assert dr.solve_with_root(0, 6) == 1
+    assert dr.solve_with_root(0, 7) == 0
