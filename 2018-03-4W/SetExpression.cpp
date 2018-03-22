4bd9cce32fcf7ab607d07e7a146840e4391185c0
Hanbyul Kim <hahanbyul@gmail.com>
2018-03-22 22:07:39 +0900

    [ADD] set_expression: online -> TIMEOUT

---
+++ 2018-03-4W/SetExpression.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class QuickUnion {
+    public:
+        QuickUnion(int _N): N(_N), id(_N, 1), size(_N, 0) {
+            for (int n = 0; n < N; ++n)
+                id[n] = n;
+        }
+
+        int find(int p) {
+            while (p != id[p]) p = id[p];
+            return p;
+        }
+
+        void connected(int p, int q) {
+            if (find(p) == find(q)) cout << "YES\n";
+            else                    cout << "NO\n";
+        }
+
+        void unite(int p, int q) {
+            int i = find(p);
+            int j = find(q);
+
+            if (i == j) return;
+
+            if (size[i] >= size[j]) {
+                id[j] = i;
+                size[i] += size[j];
+            } 
+            else {
+                id[i] = j;
+                size[j] += size[i];
+            }
+        }
+
+    private:
+        int N;
+        vector<int> id, size;
+};
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+    QuickUnion qu(n);
+    for (int i = 0; i < m; ++i) {
+        int op, a, b;
+        cin >> op >> a >> b;
+        if (op == 0) qu.unite(a, b);
+        else         qu.connected(a, b);
+    }
+}
