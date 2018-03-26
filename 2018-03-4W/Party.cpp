df0bca1d0b3b251a75a2a256128616b48a3a8b31
Hanbyul Kim <hahanbyul@gmail.com>
2018-03-26 22:12:28 +0900

    [FIX] party: not break in middle, online -> PASS (4ms)

--- 2018-03-4W/Party.cpp
+++ 2018-03-4W/Party.cpp
@@ -7,77 +7,68 @@
 
 using namespace std;
 
-const int NO_ROAD = 0;
-
 int N, M, X;
-vector<vector<pair<int, int> > > road;
+vector<vector<pair<int, int> > > normalRoad, reversedRoad;
 
-int dijkstra(int start, int goal) {
-	start -= 1;
-	goal  -= 1;
+void dijkstra(vector<int>& distances, const vector<vector<pair<int, int> > >& road) {
+    priority_queue<pair<int, int> > pq;
 
-	vector<int> distances(N, INT_MIN);
-	priority_queue<pair<int, int> > pq;
-	int minDistance = 0;
+    distances[X] = 0;
+    pq.push(make_pair(0, X));
 
-	distances[start] = 0;
-	pq.push(make_pair(0, start));
+    while (!pq.empty()) {
+        pair<int,int> p = pq.top();
+        int dist_so_far = p.first;
+        int here = p.second;
+        pq.pop();
 
-	while (!pq.empty()) {
-		pair<int,int> p = pq.top();
-		int dist_so_far = p.first;
-		int here = p.second;
-		pq.pop();
+        if (distances[here] > dist_so_far) continue;
 
-		if (here == goal) {
-			minDistance = -dist_so_far;
-			break;
-		}
-		if (distances[here] > dist_so_far) continue;
-
-		for (int i = 0; i < road[here].size(); ++i) {
+        for (int i = 0; i < road[here].size(); ++i) {
             pair<int, int> edge = road[here][i];
             int dist  = edge.first;
             int there = edge.second;
 
-			if (distances[there] < dist_so_far + dist) {
-				distances[there] = dist_so_far + dist;
-				pq.push(make_pair(distances[there], there));
-			}
-			
-		}
-	}
+            if (distances[there] < dist_so_far + dist) {
+                distances[there] = dist_so_far + dist;
+                pq.push(make_pair(distances[there], there));
+            }
 
-	return minDistance;
+        }
+    }
 }
 
 int solve() {
-	int ret = 0;
-	for (int i = 1; i <= N; ++i) {
-		if (i == X) continue;
+    vector<int> normalDist(N, INT_MIN);
+    dijkstra(normalDist, reversedRoad);
 
-		int answer = dijkstra(i, X) + dijkstra(X, i);
-		if (answer > ret) ret = answer;
-	}
+    vector<int> reversedDist(N, INT_MIN);
+    dijkstra(reversedDist, normalRoad);
 
-	return ret;
+    int ret = 0;
+    for (int i = 1; i < N; ++i)
+        ret = max(ret, -(normalDist[i] + reversedDist[i]));
+    return ret;
 }
 
 int main() {
-	cin >> N >> M >> X;
+    cin >> N >> M >> X;
+    N += 1;     // for array subscript
 
-	for (int n = 0; n < N; ++n) {
-		vector<pair<int, int> > edge;
-		road.push_back(edge);
-	}
+    for (int n = 0; n < N; ++n) {
+        vector<pair<int, int> > edge;
+        normalRoad.push_back(edge);
+        reversedRoad.push_back(edge);
+    }
 
-	for (int m = 0; m < M; ++m) {
-		int start, end, T;
-		cin >> start >> end >> T;
-		road[start-1].push_back(make_pair(-T, end-1));
-	}
+    for (int m = 0; m < M; ++m) {
+        int start, end, T;
+        cin >> start >> end >> T;
+        normalRoad[start].push_back(make_pair(-T, end));
+        reversedRoad[end].push_back(make_pair(-T, start));
+    }
 
-	cout << solve() << '\n';
+    cout << solve() << '\n';
 
     return 0;
 }
