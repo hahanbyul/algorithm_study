1b05778e1541816ed9f23fb14f2de5bfedb107fb
Hanbyul Kim <hahanbyul@gmail.com>
2018-04-01 11:31:45 +0900

    [FIX] BRICKS: for submission (but not working for ex2)

--- 2018-03-5W/BrickGame.cpp
+++ 2018-03-5W/BrickGame.cpp
@@ -50,33 +50,20 @@ Score getScore(int start, int myRange, int yourRange) {
 }
 
 Score solve(int start) {
-    cout << "START: " << start << endl;
     if (start >= bricks.size())   return Score(0, 0);
-    // if (start >= bricks.size()-3) return Score(sum(start, bricks.size() - start), 0);
-    if (cache[start] != DEFAULT) {
-        cout << "CACHED: " << "(" << cache[start].myScore << ", " << cache[start].yourScore << ")" << endl;
-        return cache[start];
-    }
+    if (cache[start] != DEFAULT)  return cache[start];
 
 
     Score ret = Score(-1e9, 0);
     for (int i = 1; i <= 3; ++i) {
         for (int j = 1; j <= 3; ++j) {
             if (start + i + j > bricks.size()) break;
-            cout << "\nSTART: " << start;
-            cout << ", i: " << i << ", j: " << j << endl;
             auto score = getScore(start, i, j);
-            cout << "SCORE: " << score.myScore << endl;
             auto optimalScore        = solve(start + i + j);
             auto anotherOptimalScore = solve(start + i);
 
-            cout << "Score (start + i + j) -> yourScore: " << score.yourScore + optimalScore.yourScore << endl;
-            cout << "Score (start + i)     -> myScore:   " << anotherOptimalScore.myScore << endl;
-
-            if (score.yourScore + optimalScore.yourScore == anotherOptimalScore.myScore) {
+            if (score.yourScore + optimalScore.yourScore == anotherOptimalScore.myScore)
                 ret = max(ret, score + optimalScore);
-                cout << "updated!! ret: (" << ret.myScore << ", " << ret.yourScore << ")" << endl;
-            }
         }
     }
 
@@ -92,6 +79,7 @@ int main() {
         int N;
         scanf("%d", &N);
 
+        bricks.clear();
         bricks = vector<int>(N);
         for (int n = 0; n < N; ++n) {
             scanf("%d", &bricks[n]);
@@ -104,6 +92,5 @@ int main() {
         }
         auto answer = solve(0);
         printf("%d\n", answer.myScore);
-        printf("%d\n", answer.yourScore);
     }
 }
