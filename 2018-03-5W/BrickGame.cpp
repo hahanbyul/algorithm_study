5d85d8aff7ee113e94b729c2041492c19a6cf5fc
Hanbyul Kim <hahanbyul@gmail.com>
2018-04-01 12:14:05 +0900

    [FIX] BRICKS: int -> unsigned long long, online -> PASS

--- 2018-03-5W/BrickGame.cpp
+++ 2018-03-5W/BrickGame.cpp
@@ -6,10 +6,10 @@
 using namespace std;
 
 struct Score {
-    int myScore;
-    int yourScore;
+    unsigned long long myScore;
+    unsigned long long yourScore;
 
-    Score(int _myScore, int _yourScore) : myScore(_myScore), 
+    Score(unsigned long long _myScore, unsigned long long _yourScore) : myScore(_myScore), 
                                           yourScore(_yourScore) {}
 
     bool operator< (const Score& _score) const {
@@ -31,11 +31,11 @@ struct Score {
     }
 };
 
-vector<int> bricks;
+vector<unsigned long long> bricks;
 vector<Score> cache;
-const Score DEFAULT(-1, -1);
+const Score DEFAULT(0, 0);
 
-int sum(int start, int num) {
+unsigned long long sum(int start, int num) {
     int ret = 0;
     for (int i = 0; i < num; ++i) {
         ret += bricks[start + i];
@@ -44,8 +44,8 @@ int sum(int start, int num) {
 }
 
 Score getScore(int start, int myRange, int yourRange) {
-    int myScore   = sum(start, myRange);
-    int yourScore = sum(start + myRange, yourRange);
+    unsigned long long myScore   = sum(start, myRange);
+    unsigned long long yourScore = sum(start + myRange, yourRange);
     return Score(myScore, yourScore);
 }
 
@@ -53,15 +53,14 @@ Score solve(int start) {
     if (start >= bricks.size())   return Score(0, 0);
     if (cache[start] != DEFAULT)  return cache[start];
 
-
-    Score ret = Score(-1e9, 0);
+    Score ret = DEFAULT;
     for (int i = 1; i <= 3; ++i) {
         for (int j = 1; j <= 3; ++j) {
             if (start + i + j > bricks.size()) break;
-            auto score = getScore(start, i, j);
             auto optimalScore        = solve(start + i + j);
             auto anotherOptimalScore = solve(start + i);
 
+            auto score = getScore(start, i, j);
             if (score.yourScore + optimalScore.yourScore == anotherOptimalScore.myScore)
                 ret = max(ret, score + optimalScore);
         }
@@ -81,10 +80,10 @@ int main() {
         scanf("%d", &N);
 
         bricks.clear();
-        bricks = vector<int>(N);
+        bricks = vector<unsigned long long>(N);
         cache.clear();
         for (int n = 0; n < N; ++n) {
-            scanf("%d", &bricks[n]);
+            scanf("%llu", &bricks[n]);
             cache.push_back(DEFAULT);
         }
 
@@ -92,7 +91,8 @@ int main() {
             int start = bricks.size() - i;
             cache[start] = Score(sum(start, i), 0);
         }
+
         auto answer = solve(0);
-        printf("%d\n", answer.myScore);
+        printf("%llu\n", answer.myScore);
     }
 }
