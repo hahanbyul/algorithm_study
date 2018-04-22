766a9d965f36c636491fefddd176dbce3c3e424b
Hanbyul Kim <hahanbyul@gmail.com>
2018-04-22 23:31:03 +0900

    [FIX] sudoku: clean ver., online -> PASS

--- 2018-04-2W/Sudoku.cpp
+++ 2018-04-2W/Sudoku.cpp
@@ -24,7 +24,6 @@ public:
 
         getPossible();
         getCount(); 
-
     }
 
     void readBoard() {
@@ -76,94 +75,13 @@ public:
         checkBox(getBoxNum(i, j), cand);
     }
 
-
-    void printRow(int i) {
-        for (int j = 0; j < 9; ++j) {
-            printChar(board[i][j]);
-            cout << " ";
-        }
-        cout << "\n";
-    }
-
     void printBoard() {
-        for (int i = 0; i < 9; ++i)
-            printRow(i);
-        // cout << "remained: " << remainedZero << "\n\n";
-    }
-
-    void printCol(int c) {
         for (int i = 0; i < 9; ++i) {
-            printChar(board[i][c]);
-            cout << "\n";
+            for (int j = 0; j < 9; ++j)
+                printf("%d ", board[i][j]);
+            printf("\n");
         }
     }
-
-    void printBox(int b) {
-        for (int i = rowPos[b]; i < rowPos[b] + 3; ++i) {
-            for (int j = colPos[b]; j < colPos[b] + 3; ++j) {
-                printChar(board[i][j]);
-                cout << " ";
-            }
-            cout << "\n";
-        }
-    }
-
-    void printRowCand(int row) {
-        for (int col = 0; col < 9; ++col) {
-            if (board[row][col] == 0) {
-                printf("(%d, %d): ", row, col);
-                printCand(marked[row][col]);
-                printf("(%d) ", possibleLength[row][col]);
-            }
-        }
-        printf("\n");
-    }
-
-    void printColCand(int col) {
-        for (int row = 0; row < 9; ++row) {
-            if (board[row][col] == 0) {
-                printf("(%d, %d): ", row, col);
-                printCand(marked[row][col]);
-                printf("(%d) ", possibleLength[row][col]);
-            }
-        }
-        printf("\n");
-    }
-
-    void printBoxCand(int box) {
-        for (int i = rowPos[box]; i < rowPos[box] + 3; ++i) {
-            for (int j = colPos[box]; j < colPos[box] + 3; ++j) {
-                if (board[i][j] == 0) {
-                    printf("(%d, %d): ", i, j);
-                    printCand(marked[i][j]);
-                    printf("(%d) ", possibleLength[i][j]);
-                }
-            }
-        }
-        printf("\n");
-    }
-
-    void printCount(const vector<int>& array) {
-        for (int k = 1; k <= 9; ++k)
-            printf("%d ", array[k]);
-        cout << "\n";
-    }
-
-    void printCountInRow(int row) {
-        printf("%d) ", row);
-        printCount(countInRow[row]);
-    }
-
-    void printCountInCol(int col) {
-        printf("%d) ", col);
-        printCount(countInCol[col]);
-    }
-
-    void printCountInBox(int box) {
-        printf("%d) ", box);
-        printCount(countInBox[box]);
-    }
-
 
     void fill(int i, int j) {
         int box = getBoxNum(i, j);
@@ -173,14 +91,12 @@ public:
         int k;
         for (k = 1; k < 10; ++k) {
             if (marked[i][j][k] != POSSIBLE) continue;
-            // printf("%d) %d %d %d\n", k, countInRow[i][k], countInCol[j][k], countInBox[box][k]);
             if (possibleLength[i][j] == 1 || countInRow[i][k] == 1 || countInCol[j][k] == 1 || countInBox[box][k] == 1) break;
         }
 
         if (k == 10) return;
 
         mark(i, j, k);
-        // printBoard();
         
         for (int n = 0; n < 9; ++n) {
             fill(i, n);
@@ -233,49 +149,15 @@ public:
                 fill(i, j);
     }
 
-    void printAllCand() {
-        cout << "row cand.: \n";
-        for (int i = 0; i < 9; ++i)
-            printRowCand(i);
-        cout << "col cand.: \n";
-        for (int i = 0; i < 9; ++i)
-            printColCand(i);
-        cout << "box cand.: \n";
-        for (int i = 0; i < 9; ++i)
-            printBoxCand(i);
-    }
-
-    void printAllCount() {
-        cout << "row count: \n";
-        for (int i = 0; i < 9; ++i)
-            printCountInRow(i);
-        cout << "col count: \n";
-        for (int i = 0; i < 9; ++i)
-            printCountInCol(i);
-        cout << "box count: \n";
-        for (int i = 0; i < 9; ++i)
-            printCountInBox(i);
-    }
-
-    void confirm() {
-        for (int i = 0; i < 9; ++i)
-            for (int j = 0; j < 9; ++j)
-                if (board[i][j] < 0) board[i][j] = -board[i][j];
-    }
-
-    bool dfs() { return dfs(remainedZero); }
-    bool dfs(int remained) {
+    bool dfs() { return dfs(0, 0, remainedZero); }
+    bool dfs(int _i, int _j, int remained) {
         // printBoard();
         if (remained == 0) return true;
         
         // find next empty
-        int i, j;
-        for (i = 0; i < 9; ++i) {
-            for (j = 0; j < 9; ++j) {
-                if (board[i][j] == 0) break;
-            }
-            if (j != 9 && board[i][j] == 0) break;
-        }
+        pair<int, int> p = findNextEmpty(_i, _j);
+        int i = p.first;
+        int j = p.second;
 
         vector<int> cand(10, POSSIBLE);
         checkRow(i, cand);
@@ -289,7 +171,7 @@ public:
             if (cand[k] != POSSIBLE) continue;
 
             board[i][j] = k;
-            bool result = dfs(remained - 1);
+            bool result = dfs(i, j, remained - 1);
             if (result) return true;
         }
 
@@ -297,20 +179,16 @@ public:
         return false;
     }
 
+    pair<int, int> findNextEmpty(int _i, int _j) {
+        for (int i = _i; i < 9; ++i)
+            for (int j = _j; j < 9; ++j)
+                if (board[i][j] == 0) return make_pair(i, j);
+        return findNextEmpty(0, 0);
+    }
+
 private:
     int getBoxNum(int i, int j) {
         return i/3*3 + j/3;
-    }
-
-    void printChar(char ch) {
-        if (ch == '0') cout << "\033[1;31m" << ch << "\033[0m";
-        else           cout << ch;
-    }
-
-    void printChar(int ch) {
-        if (ch == 0)      cout << "\033[1;31m" <<  ch << "\033[0m";
-        else if (ch <  0) cout << "\033[1;34m" << -ch << "\033[0m";
-        else              cout << ch;
     }
 
     void checkRow(int i, vector<int>& cand) {
@@ -329,11 +207,6 @@ private:
                 cand[board[i][j]] = IMPOSSIBLE;
     }
 
-    void printCand(vector<int>& cand) {
-        for (int n = 1; n < 10; ++n)
-            if (cand[n] == POSSIBLE) cout << n << " ";
-    }
-
     int getCandLength(int i, int j) {
         int ret = 0;
         for (int k = 1; k < 10; ++k)
@@ -349,7 +222,6 @@ int main() {
 
     sdk.fillDefinite();
     // sdk.printBoard();
-    // sdk.confirm();
 
     sdk.dfs();
     sdk.printBoard();
