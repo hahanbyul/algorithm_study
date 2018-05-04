1039a4c0ff447f85460f6e8f487e98ad91f3dcb1
Hanbyul Kim <hahanbyul@gmail.com>
2018-05-04 13:43:07 +0900

    [FIX] palen_dfs: + map count

--- 2018-04-2W/PalenPathDFS.cpp
+++ 2018-04-2W/PalenPathDFS.cpp
@@ -1,9 +1,12 @@
 #include <cstdio>
 #include <string>
+#include <algorithm>
+#include <map>
 using namespace std;
 
 int N;
 char board[500][500];
+map<string, int> pathLeft, pathRight;
 
 const int LEFT  = -1;
 const int RIGHT = 1;
@@ -35,7 +38,7 @@ public:
 
     Node* go(int y, int x, int direction) {
         ++count;
-        if (!isInBoard(y, x) || !hasOpposite(y, x, direction)) { 
+        if (!isInBoard(y, x) || !hasOpposite(board[y][x], direction)) {  // 끝에 도달하면 안에 안들어가도 됨
             return 0; 
         }
 
@@ -43,16 +46,20 @@ public:
         return this;
     }
 
-    bool hasOpposite(int y, int x, int direction) {
-        int yMirror = yStart - (x - xStart);
-        int xMirror = xStart - (y - yStart);
+    bool hasOpposite(char ch, int direction) {
+        int dist = count - 1;
 
-        if (board[yMirror][xMirror] == board[y][x]) { return true; }
-        if (isInMirrorRange(yMirror+1, xMirror-1, direction) && board[yMirror+1][xMirror-1] == board[y][x]) {
-            return true;
-        }
-        if (isInMirrorRange(yMirror-1, xMirror+1, direction) && board[yMirror-1][xMirror+1] == board[y][x]) {
-            return true;
+        int yDiff;
+        if (direction == RIGHT) { yDiff = min(dist, yStart); }
+        else                    { yDiff = min(dist, N - 1 - yStart); }
+
+        int y = yStart - direction * yDiff;
+        int x = xStart - direction * (dist - yDiff);
+        while (isInMirrorRange(y, x, direction)) {
+            if (board[y][x] == ch) { return true; }
+
+            y += direction;
+            x -= direction;
         }
 
         return false;
@@ -65,16 +72,20 @@ public:
     void print() {
         printf("%s\n", path.c_str());
     }
+
+    string getPath() {
+        return path;
+    }
 };
 
 void dfs(Node* curNode, int y, int x, int direction) {
     if (curNode == 0) { return; }
     if (curNode->isGoal()) { // 종료 조건
-        curNode->print();
-        // TODO: map 카운트 증가
+        // curNode->print();
+        if (direction == RIGHT) ++pathRight[curNode->getPath()];
+        else                    ++pathLeft[curNode->getPath()];
         return;
     }
-    printf("%d %d\n", y, x);
 
     int yNext = y + direction;
     dfs(curNode->go(yNext, x, direction), yNext, x, direction);
@@ -91,16 +102,27 @@ int main() {
     for (int n = 0; n < N; ++n)
         scanf("%s", board[n]);
 
-    int numOfCases = 0;
     if (board[0][0] != board[N-1][N-1]) {
         printf("0\n");
         // continue;
     }
 
+    for (int i = 0; i < N; ++i) {
+        Node rightStart(i, N-1-i);
+        dfs(&rightStart, i, N-1-i, RIGHT);
+
+        Node leftStart(i, N-1-i);
+        dfs(&leftStart, i, N-1-i, LEFT);
+    }
+
+    int numOfCases = 0;
+
+    /*
     while (true) {
         int i;
         scanf("%d", &i);
         Node start(i, N-1-i);
-        dfs(&start, i, N-1-i, LEFT);
+        dfs(&start, i, N-1-i, RIGHT);
     }
+    */
 }
