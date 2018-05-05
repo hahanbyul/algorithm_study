a8bc8a53b5aa8309e282ef930d1dc41de0d2b6dc
Hanbyul Kim <hahanbyul@gmail.com>
2018-05-05 18:06:11 +0900

    [FIX] timetrip: - floyd, online -> PASS (fastest!!)

--- 2018-05-1W/TIMETRIP.cpp
+++ 2018-05-1W/TIMETRIP.cpp
@@ -1,3 +1,6 @@
+// link:   https://algospot.com/judge/problem/read/TIMETRIP
+// result: https://cl.ly/2F0l2o460m2N
+
 #include <cstdio>
 #include <vector>
 #include <utility>
@@ -5,20 +8,38 @@
 using std::vector;
 using std::pair;
 using std::make_pair;
-using std::fill;
 
 const int INF = 200000;
-const bool REVERSED = true;
 
 int G, W;
 vector<pair<int, int> > adj[100];
-bool reachable[100][100];
 
-void bellmanFord(vector<int>& upper, bool reversed=false) {
+bool isNegCycleReachable(const vector<int>& upper, bool reversed=false) {
+    // 추가 iteration
+    for (int here = 0; here < G; ++here) {
+        if (upper[here] == INF) { continue; }
+        for (int i = 0; i < adj[here].size(); ++i) {
+            int there = adj[here][i].first;
+            int cost  = adj[here][i].second;
+            if (reversed) { cost = -cost; }
+
+            if (upper[there] > upper[here] + cost) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+bool bellmanFord(bool reversed=false) {
+    vector<int> upper(G, INF);
+    upper[0] = 0;
+
     bool updated;
     for (int iter = 0; iter < G - 1; ++iter) {
         updated = false;
         for (int here = 0; here < G; ++here) {
+            if (upper[here] == INF) { continue; }
             for (int i = 0; i < adj[here].size(); ++i) {
                 int there = adj[here][i].first;
                 int cost  = adj[here][i].second;
@@ -32,47 +53,19 @@ void bellmanFord(vector<int>& upper, bool reversed=false) {
         }
         if (!updated) break;
     }
-}
 
-bool isNegCycleReachable(const vector<int>& upper, bool reversed=false) {
-    // 추가 iteration
-    for (int here = 0; here < G; ++here) {
-        for (int i = 0; i < adj[here].size(); ++i) {
-            int there = adj[here][i].first;
-            int cost  = adj[here][i].second;
-            if (reversed) { cost = -cost; }
-
-            if (upper[there] > upper[here] + cost) {
-                if (reachable[0][here] && reachable[here][1]) {
-                    return true;
-                }
-            }
-        }
-    }
-    return false;
-}
-
-void floyd() {
-    for (int i = 0; i < G; ++i) {
-        for (int j = 0; j < G; ++j) {
-            reachable[i][j] = false;
-        }
+    if (upper[1] == INF) {
+        return false;
     }
 
-    for (int here = 0; here < G; ++here) {
-        for (int i = 0; i < adj[here].size(); ++i) {
-            int there = adj[here][i].first;
-            reachable[here][there] = true;
-        }
+    if (updated && isNegCycleReachable(upper, reversed)) {
+        printf("INFINITY");
     }
-
-    for (int k = 0; k < G; ++k) {
-        for (int i = 0; i < G; ++i) {
-            for (int j = 0; j < G; ++j) {
-                reachable[i][j] = reachable[i][j] || (reachable[i][k] && reachable[k][j]);
-            }
-        }
+    else {
+        if (!reversed) printf("%d", upper[1]);
+        else           printf("%d", -upper[1]);
     }
+    return true;
 }
 
 int main() {
@@ -92,33 +85,15 @@ int main() {
             adj[a].push_back(make_pair(b, d));
         }
 
-        floyd();
-        if (!reachable[0][1]) {
+        bool reachable = bellmanFord();
+        if (!reachable) {
             printf("UNREACHABLE\n");
             continue;
         }
 
-        vector<int> upper(G, INF);
-        upper[0] = 0;
-
-        bellmanFord(upper);
-        if (isNegCycleReachable(upper)) {
-            printf("INFINITY ");
-        }
-        else {
-            printf("%d ", upper[1]);
-        }
-
-        fill(upper.begin(), upper.end(), INF);
-        upper[0] = 0;
-
-        bellmanFord(upper, REVERSED);
-        if (isNegCycleReachable(upper, REVERSED)) {
-            printf("INFINITY\n");
-        }
-        else {
-            printf("%d\n", -upper[1]);
-        }
+        printf(" ");
+        bellmanFord(true);
+        printf("\n");
     }
 
     return 0;
