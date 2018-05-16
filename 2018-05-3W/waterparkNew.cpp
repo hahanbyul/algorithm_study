021d53a4a4fd980cc1d0572c3764a41e6fc23f9a
Hanbyul Kim <hahanbyul@gmail.com>
2018-05-16 23:53:45 +0900

    [FIX] waterpark: * submit

--- 2018-05-3W/waterparkNew.cpp
+++ 2018-05-3W/waterparkNew.cpp
@@ -4,63 +4,34 @@ using std::max;
 using std::min;
 
 int N, D;
-// int stone[100000];  // 동적할당 하는 것과 비교
+int stone[100000];          // 동적할당 하는 것과 비교
 long long cache[100000];
 
-void print(int* array) {
-    for (int i = 0; i < N; ++i) {
-        printf("%d ", array[i]);
-    }
-    printf("\n");
-}
-
-void print(long long *array) {
-    for (int i = 0; i < N; ++i) {
-        printf("%lld ", array[i]);
-    }
-    printf("\n");
-}
-
-struct Score {
-    long long value;
-    int index;
-    int count;
-
-    Score(long long _value, int _index, int _count=1) : value(_value), index(_index), count(_count) {}
-    Score(const Score& s) { value = s.value, index = s.index; }
-    bool operator<= (Score& s) { return value <= s.value; }
-    long long getPosVal() { return value > 0 ? value : 0; }
-};
-
 int main() {
-    /*
-    N = 10;
-    D = 2;
-    int stone[] = {2, 7, -5, -4, 10, -5, -5, -5, 30, -10};
-    */
+    scanf("%d", &N);
+    scanf("%d", &D);
 
-    N = 3;
-    D = 2;
-    int stone[] = {-4, -2, -7};
+    for (int i = 0; i < N; ++i) {
+        scanf("%d", &stone[i]);
+    }
 
-    print((int*) stone);
-
-    // Score globalMaxSum(stone[N-1], N-1);
+    long long globalMax = stone[N-1];
+    int globalMaxIndex  = N-1;
     cache[N-1] = stone[N-1];
     for (int start = N - 2; start >= 0; --start) {
-        long long localMax = 0;
-        for (int i = start + 1; i <= min(start + D, N-1); ++i) {
-            localMax = max(localMax, cache[i]);
+        long long localMax = (globalMax > 0 && globalMaxIndex - start <= D)? globalMax : 0;
+        if (localMax == 0 && globalMax > 0) {
+            for (int i = start + 1; i <= min(start + D, N-1); ++i) {
+                localMax = max(localMax, cache[i]);
+            }
         }
         cache[start] = stone[start] + localMax;
+
+        if (globalMax < cache[start]) {
+            globalMax = cache[start];
+            globalMaxIndex = start;
+        }
     }
 
-    long long maxSum = -1000000000;
-    for (int i = 0; i < N-1; ++i) {
-        maxSum = max(maxSum, cache[i]);
-    }
-
-    print(cache);
-
-    printf("%d\n", maxSum);
+    printf("%lld\n", globalMax);
 }
