fcf857e6f0b1c2ef1200152ebe2f7abf165b2afd
Hanbyul Kim <hahanbyul@gmail.com>
2018-05-19 00:29:34 +0900

    [FIX] geoSeq: + mod

--- 2018-05-3W/geoSeq2.cpp
+++ 2018-05-3W/geoSeq2.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int a, r, n, mod;
-unsigned long long cache[36];
+int cache[36];
 
 int toIndex(int x) {
     int index = 0;
@@ -21,36 +21,36 @@ int toIndex(int x) {
     return index;
 }
 
-unsigned long long powerOfr(int p=n, int index=0, int bias=-1) {
+int powerOfr(int p=n, int index=0, int bias=-1) {
     printf("%d %d %d\n", p, index, bias);
     if (p == 2) {
         int answer = bias == -1? r + 1 : r*r + 1;
         printf("%d\n", answer);
-        return answer;
+        return answer % mod;
     }
     if (cache[index] != 0) {
-        unsigned long long answer =  bias == -1? cache[index] : cache[index] * (r - 1) + 2;
-        printf("%lld\n", answer);
-        return answer;
+        int answer =  bias == -1? cache[index] : cache[index] * (r - 1) + 2;
+        printf("%d\n", answer);
+        return answer % mod;
     }
     if (p % 2 == 0) {
-        cache[index] = powerOfr(p/2, index + 1, -1) * powerOfr(p/2, index + 1, +1);
-        printf("%lld\n", cache[index]);
+        cache[index] = (powerOfr(p/2, index + 1, -1) * powerOfr(p/2, index + 1, +1)) % mod;
+        printf("%d\n", cache[index]);
         return cache[index];
     }
     else {
-        cache[index] = r * powerOfr(p - 1, index + 1, -1) + 1;
-        printf("%lld\n", cache[index]);
+        cache[index] = (r * powerOfr(p - 1, index + 1, -1) + 1) % mod;
+        printf("%d\n", cache[index]);
         return cache[index];
     }
 }
 
+int solve() {
+    return (a*powerOfr()) % mod;
+}
+
 int main() {
-    n = 17, r = 4;
-    int args;
-    // while (1) {
-    //     scanf("%d", &args);
-    //     printf("%d\n", toIndex(args));
-    // }
-    printf("%lld\n", powerOfr());
+    // a = 3, r = 5, n = 2, mod = 10;
+    a = 1, r = 2, n = 9, mod = 100;
+    printf("%d\n", solve());
 }
