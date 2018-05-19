564e860883ac543bfc849614ff492d42de7f9c3d
Hanbyul Kim <hahanbyul@gmail.com>
2018-05-19 18:39:30 +0900

    [FIX] particleOnTree: * submit ver., online -> TIMEOUT

--- 2018-05-3W/particleOnTree.cpp
+++ 2018-05-3W/particleOnTree.cpp
@@ -1,13 +1,13 @@
+#include <cstdio>
 #include <vector>
-#include <iostream>
 using namespace std;
 
 int N, M;
 vector<vector<int> > adj;
 vector<bool> visited;
 
-unsigned int possibleStart[2];
-unsigned int possibleGoal[2];
+unsigned long long possibleStart[2];
+unsigned long long possibleGoal[2];
 
 const int RED = 0;
 const int BLACK = 1;
@@ -18,8 +18,7 @@ void addEdge(int u, int v) {
     adj[v].push_back(u);
 }
 
-void dfs(int u, int height, unsigned int array[2]) {
-    cout << u + 1 << " " << height << endl;
+void dfs(int u, int height, unsigned long long array[2]) {
     visited[u] = true;
     ++array[height % 2];
 
@@ -31,34 +30,36 @@ void dfs(int u, int height, unsigned int array[2]) {
     }
 }
 
-unsigned int solve(int u, int v, int color) {
+unsigned long long solve(int u, int v, int color) {
     --u, --v;
     visited = vector<bool>(N, false);
     visited[u] = true;
     possibleGoal[0] = 0, possibleGoal[1] = 0;
     dfs(v, 1, possibleGoal);
-    cout << "=> " << possibleGoal[0] << " " << possibleGoal[1] << endl;
 
     visited[u] = false;
     possibleStart[0] = 0, possibleStart[1] = 0;
     dfs(u, 0, possibleStart);
-    cout << "=> " << possibleStart[0] << " " << possibleStart[1] << endl;
 
     return color == RED? possibleStart[RED] * possibleGoal[RED] + possibleStart[BLACK] * possibleGoal[BLACK] :
                          possibleStart[RED] * possibleGoal[BLACK] + possibleStart[BLACK] * possibleGoal[RED];
 }
 
 int main() {
-    N = 6;
+    scanf("%d %d", &N, &M);
     adj = vector<vector<int> >(N);
-    addEdge(2, 4);
-    addEdge(2, 5);
-    addEdge(2, 6);
-    addEdge(1, 2);
-    addEdge(3, 1);
 
-    cout << solve(1, 3, RED) << endl;
-    cout << solve(1, 3, BLACK) << endl;
-    cout << solve(2, 1, 1) << endl;
+    for (int n = 0; n < N-1; ++n) {
+        int u, v;
+        scanf("%d %d", &u, &v);
+        addEdge(u, v);
+    }
+
+    for (int m = 0; m < M; ++m) {
+        int u, v, c;
+        scanf("%d %d %d", &u, &v, &c);
+        printf("%lld\n", solve(u, v, c));
+    }
+
     return 0;
 }
